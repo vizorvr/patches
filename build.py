72996a1c8b13de768359990dda53f0845f34232d
Lasse Jul Nielsen <lasse@effekts.dk>
2011-12-18 12:55:55 +0100

    Updates to the build script: Compress / concatenate plugins. Copy structure theme files.

--- build.py
+++ build.py
@@ -28,3 +28,38 @@ scripts = map(lambda x: x[len(scripts_path):], glob.glob(scripts_path + '*.js'))
 for script in scripts:
 	print '\tCompressing ' + script
 	os.system('yui-compressor --type js --preserve-semi -o ' + build_dir + '/' + scripts_path + script + ' ./' + scripts_path + script)
+	
+print 'Copying structure themes...'
+themes_path = scripts_path + 'themes'
+shutil.copytree(themes_path, build_dir + '/' + themes_path)
+
+print 'Compressing plugins...'
+plugins_path = 'plugins/'
+os.mkdir(build_dir + '/' + plugins_path)
+plugins = map(lambda x: x[len(plugins_path):], glob.glob(plugins_path + '*.js'))
+
+for plugin in plugins:
+	print '\tCompressing ' + plugin
+	os.system('yui-compressor --type js --preserve-semi -o ' + build_dir + '/' + plugins_path + plugin + ' ./' + plugins_path + plugin)
+
+print 'Concatenating plugins...'
+
+plugin_data = []
+
+for plugin in plugins:
+	'Munching ' + plugin
+	
+	for line in open(plugins_path + plugin, 'r'):
+		plugin_data.append(line)
+
+plugs_concat_filename = build_dir + '/' + plugins_path + 'all.plugins'
+plugs_concat_file = open(plugs_concat_filename, 'w')
+plugs_concat_file.write(''.join(plugin_data))
+plugs_concat_file.close()
+
+print 'Reobfuscating merged plugin package...'
+
+os.system('yui-compressor --type js --preserve-semi -o ' + plugs_concat_filename + '.js ./' + plugs_concat_filename)
+os.remove(plugs_concat_filename)
+
+
