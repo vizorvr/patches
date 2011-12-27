d48f838e1d29fe4d46ec4a16e5d376f61420d949
Lasse Jul Nielsen <lasse@effekts.dk>
2011-12-27 15:46:50 +0100

    Added plugin: Convert camera to individual matrices.

---
+++ plugins/convert_camera_matrices.plugin.js
@@ -0,0 +1,27 @@
+g_Plugins["convert_camera_matrices"] = function(core) {
+	var self = this;
+	var gl = core.renderer.context;
+	
+	this.input_slots = [ 
+		{ name: 'camera', dt: core.datatypes.CAMERA },
+	];
+	this.output_slots = [ 
+		{ name: 'projection', dt: core.datatypes.MATRIX },
+		{ name: 'view', dt: core.datatypes.MATRIX } 
+	];
+	this.state = null;
+	this.camera = new Camera(gl);
+	
+	this.update_input = function(index, data)
+	{
+		self.camera = data;
+	};	
+
+	this.update_output = function(index)
+	{
+		if(index === 0)
+			return self.camera.projection;
+		else if(index === 1)
+			return self.camera.view;
+	};	
+};
