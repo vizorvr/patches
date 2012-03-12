8ba2765acf7afd177e04ac1a377247fa88065f6c
Lasse Jul Nielsen <lasse@effekts.dk>
2012-03-12 18:30:20 +0100

    Added color display plugin.

---
+++ plugins/color_display.plugin.js
@@ -0,0 +1,40 @@
+E2.plugins["color_display"] = function(core, node) {
+	var self = this;
+	
+	this.input_slots = [ 
+		{ name: 'color', dt: core.datatypes.COLOR }
+	];
+	
+	this.output_slots = [];
+
+	this.reset = function()
+	{
+		self.update_value(1.0, 1.0, 1.0);
+	};
+	
+	this.create_ui = function()
+	{
+		self.label = make('span');
+		self.label.html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;') 
+		self.label.css('background-color', '#fff');
+		self.label.css('border', '1px #aaa solid');
+		
+		return self.label;
+	};
+	
+	this.disconnected = function(slot)
+	{
+		self.update_value(0);
+	};
+
+	this.update_input = function(slot, data)
+	{
+		self.update_value(data.rgba[0], data.rgba[1], data.rgba[2]);
+	};
+	
+	this.update_value = function(r, g, b)
+	{
+		if(self.label)
+			self.label.css('background-color', 'rgb(' + Math.round(r * 255.0) + ', ' + Math.round(g * 255.0) + ', ' + Math.round(b * 255.0) + ')');
+	};
+};
