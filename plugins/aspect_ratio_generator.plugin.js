ab876df63f446848d7e9f71dfb7909e722837677
Lasse Jul Nielsen <lasse@effekts.dk>
2012-05-31 15:57:56 +0200

    Added new plugin aspect ratio generator. Emits renderer x/y (typically 1.33 for 4:3), which can be used for aspect correction.

---
+++ plugins/aspect_ratio_generator.plugin.js
@@ -0,0 +1,14 @@
+E2.plugins["aspect_ratio_generator"] = function(core, node) {
+	var self = this;
+	
+	this.desc = 'Emits current renderer aspect ratio (width / height).';
+	this.input_slots = [];
+	this.output_slots = [ { name: 'aspect', dt: core.datatypes.FLOAT } ];
+	
+	this.update_output = function(slot)
+	{
+		var c = core.renderer.canvas;
+
+		return c.width() / c.height();
+	};
+};
