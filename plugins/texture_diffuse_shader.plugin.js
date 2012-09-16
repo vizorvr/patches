1e5f909cb90c4b1de68ecc7a177c1271ebd04633
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-09-16 18:40:02 +0200

    Changed the texture shader to expect seperate uv offset / scale / rotation, instead of a 4x4 matrix.

--- plugins/texture_diffuse_shader.plugin.js
+++ plugins/texture_diffuse_shader.plugin.js
@@ -8,8 +8,10 @@ E2.plugins["texture_diffuse_shader"] = function(core, node) {
 		 { name: 'is3d', dt: core.datatypes.BOOL, desc: 'En- or disable depth buffer write and masking.', def: 'False' },
 		 { name: 'color', dt: core.datatypes.COLOR, desc: 'Diffuse surface color.', def: 'White' },
 		 { name: 'blend mode', dt: core.datatypes.FLOAT, desc: 'Fragment blend mode.', def: 'Normal' },
-		 { name: 'tex transform', dt: core.datatypes.TRANSFORM, desc: 'Texture transform supplied as an 4x4 matrix. All operation should be constrained to the XY plane.', def: 'Identity' },
-		 { name: 'texture', dt: core.datatypes.TEXTURE, desc: 'Diffuse texture map.' }
+		 { name: 'texture', dt: core.datatypes.TEXTURE, desc: 'Diffuse texture map.' },
+		 { name: 'uv offset', dt: core.datatypes.VERTEX, desc: 'UV translation. Only the x and y components are used with the z axis disregarded.' },
+		 { name: 'uv scale', dt: core.datatypes.VERTEX, desc: 'UV scale. Only the x and y components are used with the z axis factor disregarded.' },
+		 { name: 'uv rotation', dt: core.datatypes.FLOAT, desc: 'UV rotation in degrees.' },
 	];
 	
 	this.output_slots = [ 
@@ -19,14 +21,17 @@ E2.plugins["texture_diffuse_shader"] = function(core, node) {
 	var vs_src = 'attribute vec3 pos;' +
 		     'attribute vec2 uv;' +
 		     'varying vec2 uv_coord;' +
-		     'uniform mat4 t_mat;' +
 		     'uniform mat4 m_mat;' +
 		     'uniform mat4 v_mat;' +
 		     'uniform mat4 p_mat;' +
+		     'uniform vec2 uv_offset;' +
+		     'uniform vec2 uv_scale;' +
+		     'uniform float uv_rotation;' +
 		     'void main(void) {' +
-		     '    vec4 t = vec4(uv.x, uv.y, 0.0, 0.0);' +
 		     '    gl_Position = p_mat * v_mat * m_mat * vec4(pos, 1.0);' + 
-		     '    uv_coord = (t * t_mat).xy;' +
+		     '    float cs = cos(uv_rotation);' +
+		     '    float sn = sin(uv_rotation);' +
+		     '    uv_coord = vec2((((uv.x * cs) - (uv.y * sn)) * uv_scale.x) + uv_offset.x, (((uv.x * sn) + (uv.y * cs)) * uv_scale.y) + uv_offset.y);' +
 		     '}';
 		
 	var ps_src = 'precision mediump float;' +
@@ -47,12 +52,14 @@ E2.plugins["texture_diffuse_shader"] = function(core, node) {
 	
 	this.s.vertexPosAttribute = gl.getAttribLocation(prog, "pos");
 	this.s.uvCoordAttribute = gl.getAttribLocation(prog, "uv");
-	this.s.tMatUniform = gl.getUniformLocation(prog, "t_mat");
 	this.s.mMatUniform = gl.getUniformLocation(prog, "m_mat");
 	this.s.vMatUniform = gl.getUniformLocation(prog, "v_mat");
 	this.s.pMatUniform = gl.getUniformLocation(prog, "p_mat");
 	this.s.colorUniform = gl.getUniformLocation(prog, "color");
 	this.s.tex0Uniform = gl.getUniformLocation(prog, "tex0");
+	this.s.uvOffsetUniform = gl.getUniformLocation(prog, "uv_offset");
+	this.s.uvScaleUniform = gl.getUniformLocation(prog, "uv_scale");
+	this.s.uvRotationUniform = gl.getUniformLocation(prog, "uv_rotation");
 	
 	// Note: Shader implementations must decorate the shader prototype with these
 	// two methods, so the rendering plugins can call it to update the shader uniforms
@@ -85,6 +92,9 @@ E2.plugins["texture_diffuse_shader"] = function(core, node) {
 		gl.enableVertexAttribArray(self.s.uvCoordAttribute);
 		
 		gl.uniformMatrix4fv(self.s.tMatUniform, false, self.texture_transform);			
+		gl.uniform2fv(self.s.uvOffsetUniform, self.uv_offset);	
+		gl.uniform2fv(self.s.uvScaleUniform, self.uv_scale);	
+		gl.uniform1f(self.s.uvRotationUniform, self.uv_rotation);
 
 		if(self.tex !== null)
 		{
@@ -117,9 +127,13 @@ E2.plugins["texture_diffuse_shader"] = function(core, node) {
 		else if(slot.index === 2)
 			self.blend_mode = data;
 		else if(slot.index === 3)
-			self.texture_transform = data;
-		else if(slot.index === 4)
 			self.tex = data;
+		else if(slot.index === 4)
+			self.uv_offset = new Float32Array([data[0], data[1]]);
+		else if(slot.index === 5)
+			self.uv_scale = new Float32Array([data[0], data[1]]);
+		else if(slot.index === 6)
+			self.uv_rotation = ((data % 360.0) / 180.0) * Math.PI;
 	};
 	
 	this.update_output = function(slot)
@@ -136,6 +150,9 @@ E2.plugins["texture_diffuse_shader"] = function(core, node) {
 			self.blend_mode = core.renderer.blend_mode.NORMAL;
 			self.tex = null;
 			self.texture_transform = mat4.create();
+			self.uv_offset = new Float32Array([0.0, 0.0]);
+			self.uv_scale = new Float32Array([1.0, 1.0]);
+			self.uv_rotation = 0.0;
 			
 			mat4.identity(self.texture_transform);
 		}
