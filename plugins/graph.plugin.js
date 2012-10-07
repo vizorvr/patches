89c4b144a2aba1d4f5f775b5a8fe31a6f3124415
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-10-07 12:22:30 +0200

    Renderer bug:
    Fixed wrong texture flags for render targets - they used wrapped, not
    edge clamped sampling which caused edge artifacts which are now gone.

--- plugins/graph.plugin.js
+++ plugins/graph.plugin.js
@@ -118,6 +118,8 @@ E2.plugins["graph"] = function(core, node) {
 			gl.bindTexture(gl.TEXTURE_2D, self.texture);
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, self.framebuffer.width, self.framebuffer.height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
 
 			self.renderbuffer = gl.createRenderbuffer();
