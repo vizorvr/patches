26d87f6adf4805230d61ac71d1325bdc2d8b7d4b
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-28 00:08:17 +0100

    Refactored the equals plugin to use prototype.

--- plugins/equals_modulator.plugin.js
+++ plugins/equals_modulator.plugin.js
@@ -1,36 +1,38 @@
-E2.plugins["equals_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["equals_modulator"] = function(core, node)
+{
 	this.desc = 'Emits true if <b>value</b> equals <b>reference</b> and false otherwise.';
+	
 	this.input_slots = [ 
 		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'Input value.', def: 0 },
 		{ name: 'reference', dt: core.datatypes.FLOAT, desc: 'Reference value to comprate <b>value</b> to.', def: 0 } 
 	];
 	
-	this.output_slots = [ { name: 'bool', dt: core.datatypes.BOOL, desc: 'True if <b>value</b> equals <b>reference</b> and false otherwise.', def: 'False' } ];
-	
-	this.reset = function()
-	{
-		self.value = 0.0;
-		self.ref = 0.0;
-		self.state = false;
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		if(slot.index === 0)
-			self.value = data;
-		else
-			self.ref = data;
-	};	
+	this.output_slots = [
+		{ name: 'bool', dt: core.datatypes.BOOL, desc: 'True if <b>value</b> equals <b>reference</b> and false otherwise.', def: 'False' }
+	];
+};
 
-	this.update_state = function(delta_t)
-	{
-		self.state = self.value === self.ref;
-	};
-	
-	this.update_output = function(slot)
-	{
-		return self.state;
-	};	
+E2.p.prototype.reset = function()
+{
+	this.value = 0.0;
+	this.ref = 0.0;
+	this.state = false;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	if(slot.index === 0)
+		this.value = data;
+	else
+		this.ref = data;
+};	
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	this.state = this.value === this.ref;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.state;
 };
