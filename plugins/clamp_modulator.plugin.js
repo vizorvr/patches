ca002d8283720c46c9462ccff1f47b8019332395
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 14:59:25 +0100

    Refactored the clamp plugin to use prototype.

--- plugins/clamp_modulator.plugin.js
+++ plugins/clamp_modulator.plugin.js
@@ -1,52 +1,54 @@
-E2.plugins["clamp_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["clamp_modulator"] = function(core, node)
+{
 	this.desc = 'Emit a float <b>value</b> no less than <b>min</b> and no greater than <b>max</b>.';
+	
 	this.input_slots = [ 
 		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'Value to be clipped.', def: 0 },
 		{ name: 'min', dt: core.datatypes.FLOAT, desc: 'Minimum output value.', def: 0 },
 		{ name: 'max', dt: core.datatypes.FLOAT, desc: 'Maximum output value.', def: 1 } 
 	];
 	
-	this.output_slots = [ { name: 'result', dt: core.datatypes.FLOAT, desc: 'Emits <b>min</b> <= <b>value</b> >= <b>max</b>.', def: 0 } ];
+	this.output_slots = [
+		{ name: 'result', dt: core.datatypes.FLOAT, desc: 'Emits <b>min</b> <= <b>value</b> >= <b>max</b>.', def: 0 }
+	];
+};
 
-	this.reset = function()
-	{
-		self.input_val = 0.0;
-		self.min_value = 0.0;
-		self.max_value = 1.0;
-		self.output_val = 0.0;
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		if(slot.index === 0)
-			self.input_val = data;
-		else if(slot.index === 1)
-			self.min_value = data;
-		else
-			self.max_value = data;
-	};	
+E2.p.prototype.reset = function()
+{
+	this.input_val = 0.0;
+	this.min_value = 0.0;
+	this.max_value = 1.0;
+	this.output_val = 0.0;
+};
 
-	this.update_state = function(delta_t)
-	{
-		var l = self.min_value,
-		    h = self.max_value,
-		    i = self.input_val;
-		
-		if(l > h)
-		{
-			var t = l;
-			
-			l = h;
-			h = t;
-		}
-		
-		self.output_val = i < l ? l : i > h ? h : i;
-	};
+E2.p.prototype.update_input = function(slot, data)
+{
+	if(slot.index === 0)
+		this.input_val = data;
+	else if(slot.index === 1)
+		this.min_value = data;
+	else
+		this.max_value = data;
+};	
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	var l = this.min_value,
+	    h = this.max_value,
+	    i = this.input_val;
 	
-	this.update_output = function(slot)
+	if(l > h)
 	{
-		return self.output_val;
-	};	
+		var t = l;
+		
+		l = h;
+		h = t;
+	}
+	
+	this.output_val = i < l ? l : i > h ? h : i;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.output_val;
 };
