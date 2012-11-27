fcb6ab9c3c7c3f0cd9cdb4c3cf1f4b6e2c05ea09
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 14:52:35 +0100

    Refactoring of the add modulator plugin to use prototype. The last commit message was in error. That commit contained the equivalent changes for the accumulate modulator.

--- plugins/add_modulator.plugin.js
+++ plugins/add_modulator.plugin.js
@@ -1,36 +1,39 @@
-E2.plugins["add_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["add_modulator"] = function(core, node)
+{
 	this.desc = 'Add two floating point values.';
+	
 	this.input_slots = [ 
 		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'The first operand.', def: 0 },
 		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'The second operand.', def: 0 } 
 	];
 	
-	this.output_slots = [ { name: 'result', dt: core.datatypes.FLOAT, desc: 'The result of <b>first</b> + <b>second</b>.', def: 0 } ];
-	
-	this.reset = function()
-	{
-		self.input_val = 0.0;
-		self.add_val = 0.0;
-		self.output_val = 0.0;
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		if(slot.index === 0)
-			self.input_val = data;
-		else
-			self.add_val = data;
-	};	
-
-	this.update_state = function(delta_t)
-	{
-		self.output_val = self.input_val + self.add_val;
-	};
-	
-	this.update_output = function(slot)
-	{
-		return self.output_val;
-	};	
+	this.output_slots = [ 
+		{ name: 'result', dt: core.datatypes.FLOAT, desc: 'The result of <b>first</b> + <b>second</b>.', def: 0 }
+	];
 };
+
+E2.p.prototype.reset = function()
+{
+	this.input_val = 0.0;
+	this.add_val = 0.0;
+	this.output_val = 0.0;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	if(slot.index === 0)
+		this.input_val = data;
+	else
+		this.add_val = data;
+};	
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	this.output_val = this.input_val + this.add_val;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.output_val;
+};
+
