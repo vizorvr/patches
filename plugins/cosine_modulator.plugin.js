d83465ae2ce82545ff8115a9267027357421c6ca
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-28 00:05:57 +0100

    Refactored the cosine oscillator plugin to use prototype.

--- plugins/cosine_modulator.plugin.js
+++ plugins/cosine_modulator.plugin.js
@@ -1,28 +1,33 @@
-E2.plugins["cosine_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["cosine_modulator"] = function(core, node)
+{
 	this.desc = 'Cosine oscilator. A <b>time</b> value incrementing by one unit per second will yield a 1Hz output signal.';
-	this.input_slots = [ { name: 'time', dt: core.datatypes.FLOAT, desc: 'Time in seconds', def: 0 } ];
-	this.output_slots = [ { name: 'value', dt: core.datatypes.FLOAT, desc: 'Emits cos(<b>time</b> * 2pi)', def: 0 } ];
-
-	this.reset = function()
-	{
-		self.time = 0.0;
-		self.result = 0.0;
-	};
 	
-	this.update_input = function(slot, data)
-	{
-		self.time = data;
-	};	
-
-	this.update_state = function(delta_t)
-	{
-		self.result = Math.cos(self.time * 2.0 * Math.PI);
-	};
+	this.input_slots = [
+		{ name: 'time', dt: core.datatypes.FLOAT, desc: 'Time in seconds', def: 0 }
+	];
 	
-	this.update_output = function(slot)
-	{
-		return self.result;
-	};	
+	this.output_slots = [
+		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'Emits cos(<b>time</b> * 2pi)', def: 0 }
+	];
+};
+
+E2.p.prototype.reset = function()
+{
+	this.time = 0.0;
+	this.result = 0.0;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.time = data;
+};	
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	this.result = Math.cos(this.time * 2.0 * Math.PI);
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.result;
 };
