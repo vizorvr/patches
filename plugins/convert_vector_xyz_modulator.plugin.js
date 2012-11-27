fff0e11a5a3806139a7344a9779fbd5b00c486b3
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-28 00:05:11 +0100

    Refactored the vector -> xyz plugin to use prototype.

--- plugins/convert_vector_xyz_modulator.plugin.js
+++ plugins/convert_vector_xyz_modulator.plugin.js
@@ -1,7 +1,7 @@
-E2.plugins["convert_vector_xyz_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["convert_vector_xyz_modulator"] = function(core, node)
+{
 	this.desc = 'Decompose a vector to its individual XYZ components so they can be manipulated individually.';
+	
 	this.input_slots = [
 		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'Input vector.', def: '0, 0, 0' } 
 	];
@@ -11,19 +11,19 @@ E2.plugins["convert_vector_xyz_modulator"] = function(core, node) {
 		 { name: 'y', dt: core.datatypes.FLOAT, desc: 'The y-component.', def: 0 },
 		 { name: 'z', dt: core.datatypes.FLOAT, desc: 'The z-component.', def: 0 }
 	];
-	
-	this.reset = function()
-	{
-		self.value = [0.0, 0.0, 0.0];
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		self.value = data;
-	};
-	
-	this.update_output = function(slot)
-	{
-		return self.value[slot.index];
-	};
+};
+
+E2.p.prototype.reset = function()
+{
+	this.value = [0.0, 0.0, 0.0];
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.value = data;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.value[slot.index];
 };
