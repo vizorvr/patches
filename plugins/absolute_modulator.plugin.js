9aa038f8739eb681c2aa6c7d07369f6d51446df9
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 14:50:09 +0100

    Refactored the absolute modulator plugin to use prototype.

--- plugins/absolute_modulator.plugin.js
+++ plugins/absolute_modulator.plugin.js
@@ -1,28 +1,33 @@
-E2.plugins["absolute_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["absolute_modulator"] = function(core, node)
+{
 	this.desc = 'Emit the absolute value of the input.';
-	this.input_slots = [ { name: 'value', dt: core.datatypes.FLOAT, desc: 'Positive or negative input value', def: 0 } ];
-	this.output_slots = [ { name: 'result', dt: core.datatypes.FLOAT, desc: 'Positive output value', def: 0 } ];
 	
-	this.reset = function()
-	{
-		self.input_val = 0.0;
-		self.output_val = 0.0;
-	};
+	this.input_slots = [ 
+		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'Positive or negative input value', def: 0 }
+	];
 	
-	this.update_input = function(slot, data)
-	{
-		self.input_val = data;
-	};	
+	this.output_slots = [
+		{ name: 'result', dt: core.datatypes.FLOAT, desc: 'Positive output value', def: 0 }
+	];
+};
 
-	this.update_state = function(delta_t)
-	{
-		self.output_val = Math.abs(self.input_val);
-	};
-	
-	this.update_output = function(slot)
-	{
-		return self.output_val;
-	};	
+E2.p.prototype.reset = function()
+{
+	this.input_val = 0.0;
+	this.output_val = 0.0;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.input_val = data;
+};	
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	this.output_val = Math.abs(this.input_val);
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.output_val;
 };
