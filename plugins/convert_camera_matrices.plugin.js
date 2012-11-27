f5f8fdfa16531e4304b02518953f2d86041b95ca
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 19:38:29 +0100

    Refactored the convert camera to matrices to use prototype.

--- plugins/convert_camera_matrices.plugin.js
+++ plugins/convert_camera_matrices.plugin.js
@@ -1,31 +1,33 @@
-E2.plugins["convert_camera_matrices"] = function(core, node) {
-	var self = this;
-	var gl = core.renderer.context;
-	
+E2.p = E2.plugins["convert_camera_matrices"] = function(core, node)
+{
 	this.desc = 'Extract the projection and view matrices from a camera.';
+	
 	this.input_slots = [ 
 		{ name: 'camera', dt: core.datatypes.CAMERA, desc: 'The input camera to be split into constituent matrices.', def: 'Screenspace camera' },
 	];
+	
 	this.output_slots = [ 
 		{ name: 'projection', dt: core.datatypes.MATRIX, desc: 'The camera projection matrix.', def: 'Identity' },
 		{ name: 'view', dt: core.datatypes.MATRIX, desc: 'The camera view matrix.', def: 'Identity' }
 	];
 	
-	this.reset = function()
-	{
-		self.camera = new Camera(gl);
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		self.camera = data;
-	};	
+	this.gl = core.renderer.context;
+};
 
-	this.update_output = function(slot)
-	{
-		if(slot.index === 0)
-			return self.camera.projection;
-		else
-			return self.camera.view;
-	};	
+E2.p.prototype.reset = function()
+{
+	this.camera = new Camera(this.gl);
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.camera = data;
+};	
+
+E2.p.prototype.update_output = function(slot)
+{
+	if(slot.index === 0)
+		return this.camera.projection;
+	else
+		return this.camera.view;
 };
