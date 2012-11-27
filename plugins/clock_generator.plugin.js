d2383a651085f56ffa98de052bc8688235c3d9e0
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 15:00:15 +0100

    Refactored the clock plugin to use prototype.

--- plugins/clock_generator.plugin.js
+++ plugins/clock_generator.plugin.js
@@ -1,17 +1,22 @@
-E2.plugins["clock_generator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["clock_generator"] = function(core, node)
+{
 	this.desc = 'Emits current absolute time in seconds.';
+	
 	this.input_slots = [];
-	this.output_slots = [ { name: 'seconds', dt: core.datatypes.FLOAT, desc: 'Current absolute time.' } ];
 	
-	this.update_state = function(delta_t)
-	{
-		self.updated = true;
-	};
+	this.output_slots = [ 
+		{ name: 'seconds', dt: core.datatypes.FLOAT, desc: 'Current absolute time.' }
+	];
 	
-	this.update_output = function(slot)
-	{
-		return core.abs_t;
-	};
+	this.core = core;
+};
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	this.updated = true;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.core.abs_t;
 };
