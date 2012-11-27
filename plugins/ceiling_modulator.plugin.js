c1392050a1c38b7bab8feae611a207b6c91116bf
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 14:59:02 +0100

    Refactored the ceiling modulator to use prototype.

--- plugins/ceiling_modulator.plugin.js
+++ plugins/ceiling_modulator.plugin.js
@@ -1,26 +1,28 @@
-E2.plugins["ceiling_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["ceiling_modulator"] = function(core, node)
+{
 	this.desc = 'Round <b>value</b> to the closest higher interger.';
+	
 	this.input_slots = [ 
 		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'Value to be rounded.', def: 0 }
 	];
 	
-	this.output_slots = [ { name: 'result', dt: core.datatypes.FLOAT, desc: 'The rounded result.', def: 0 } ];
-	
-	this.reset = function()
-	{
-		self.value = 0;
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		if(slot.index === 0)
-			self.value = Math.ceil(data);
-	};	
-	
-	this.update_output = function(slot)
-	{
-		return self.value;
-	};	
+	this.output_slots = [
+		{ name: 'result', dt: core.datatypes.FLOAT, desc: 'The rounded result.', def: 0 }
+	];
+};
+
+E2.p.prototype.reset = function()
+{
+	this.value = 0;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	if(slot.index === 0)
+		this.value = Math.ceil(data);
+};	
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.value;
 };
