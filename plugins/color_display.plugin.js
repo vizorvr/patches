810c18006dfc2a3a664fe0e0d49daf5ff547f307
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 15:01:48 +0100

    Refactored the color display plugin to use prototype.

--- plugins/color_display.plugin.js
+++ plugins/color_display.plugin.js
@@ -1,41 +1,42 @@
-E2.plugins["color_display"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["color_display"] = function(core, node)
+{
 	this.desc = 'Displays the supplied color in a rectangle on the plugin.';
+	
 	this.input_slots = [ 
 		{ name: 'color', dt: core.datatypes.COLOR, desc: 'Input color to be displayed.', def: 'White' }
 	];
+	
 	this.output_slots = [];
+};
 
-	this.reset = function()
-	{
-		self.update_value(1.0, 1.0, 1.0);
-	};
-	
-	this.create_ui = function()
-	{
-		self.label = make('span');
-		self.label.html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;') 
-		self.label.css('background-color', '#fff');
-		self.label.css('border', '1px #aaa solid');
-		
-		return self.label;
-	};
-	
-	this.connection_changed = function(on, conn, slot)
-	{
-		if(!on)
-			self.update_value(0);
-	};
+E2.p.prototype.reset = function()
+{
+	this.update_value(1.0, 1.0, 1.0);
+};
 
-	this.update_input = function(slot, data)
-	{
-		self.update_value(data.rgba[0], data.rgba[1], data.rgba[2]);
-	};
+E2.p.prototype.create_ui = function()
+{
+	this.label = make('span');
+	this.label.html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;') 
+	this.label.css('background-color', '#fff');
+	this.label.css('border', '1px #aaa solid');
 	
-	this.update_value = function(r, g, b)
-	{
-		if(self.label)
-			self.label.css('background-color', 'rgb(' + Math.round(r * 255.0) + ', ' + Math.round(g * 255.0) + ', ' + Math.round(b * 255.0) + ')');
-	};
+	return this.label;
+};
+
+E2.p.prototype.connection_changed = function(on, conn, slot)
+{
+	if(!on)
+		this.update_value(0);
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.update_value(data.rgba[0], data.rgba[1], data.rgba[2]);
+};
+
+E2.p.prototype.update_value = function(r, g, b)
+{
+	if(this.label)
+		this.label.css('background-color', 'rgb(' + Math.round(r * 255.0) + ', ' + Math.round(g * 255.0) + ', ' + Math.round(b * 255.0) + ')');
 };
