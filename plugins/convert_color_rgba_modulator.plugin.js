1bde707b9200e3566415750101cd9a5a0e588e8f
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-28 00:02:53 +0100

    Refactored the color -> RGA plugin to use prototype.

--- plugins/convert_color_rgba_modulator.plugin.js
+++ plugins/convert_color_rgba_modulator.plugin.js
@@ -1,7 +1,7 @@
-E2.plugins["convert_color_rgba_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["convert_color_rgba_modulator"] = function(core, node)
+{
 	this.desc = 'Convert a color to its individual RGBA components so they can be individually manipulated.';
+	
 	this.input_slots = [
 		{ name: 'color', dt: core.datatypes.COLOR, desc: 'Input color to be split.', def: 'White' } 
 	];
@@ -12,19 +12,19 @@ E2.plugins["convert_color_rgba_modulator"] = function(core, node) {
 		 { name: 'blue', dt: core.datatypes.FLOAT, desc: 'Blue channel value.', def: 1 },
 		 { name: 'alpha', dt: core.datatypes.FLOAT, desc: 'Alpha channel value.', def: 1 }
 	];
-	
-	this.reset = function()
-	{
-		self.color = new Color(1.0, 1.0, 1.0, 1.0);
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		self.color = data;
-	};
-	
-	this.update_output = function(slot)
-	{
-		return self.color.rgba[slot.index];
-	};
+};
+
+E2.p.prototype.reset = function()
+{
+	this.color = new Color(1.0, 1.0, 1.0, 1.0);
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.color = data;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.color.rgba[slot.index];
 };
