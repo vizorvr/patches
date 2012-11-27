4a99b2c28ebc9eae8a85312cf1b2cdc7759bfe2a
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-28 00:07:39 +0100

    Refactored the depth function plugin to use prototype. Fixed bug that prevented the ALWAYS setting from working correctly.

--- plugins/depth_func_generator.plugin.js
+++ plugins/depth_func_generator.plugin.js
@@ -1,58 +1,62 @@
-E2.plugins["depth_func_generator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["depth_func_generator"] = function(core, node)
+{
 	this.desc = 'Select z-buffer depth compare function.';
+	
 	this.input_slots = [];
-	this.output_slots = [ { name: 'func', dt: core.datatypes.FLOAT, desc: 'Emits the selected depth function when requested or the selection state changes.', def: 'Diffuse color' } ];
+	
+	this.output_slots = [
+		{ name: 'func', dt: core.datatypes.FLOAT, desc: 'Emits the selected depth function when requested or the selection state changes.', def: 'Diffuse color' }
+	];
+	
 	this.state = { depth_func: Material.depth_func.LEQUAL };
 	this.changed = true;
-		
-	this.reset = function()
-	{
-		self.updated = true;
-	};
+};
 	
-	this.create_ui = function()
-	{
-		var df = Material.depth_func;
-		var inp = $('<select />', { selectedIndex: 4 });
-		
-		$('<option />', { value: df.NEVER, text: 'Never' }).appendTo(inp);
-		$('<option />', { value: df.LESS, text: 'Less' }).appendTo(inp);
-		$('<option />', { value: df.EQUAL, text: 'Equal' }).appendTo(inp);
-		$('<option />', { value: df.LEQUAL, text: 'Less / equal' }).appendTo(inp);
-		$('<option />', { value: df.GREATER, text: 'Greater' }).appendTo(inp);
-		$('<option />', { value: df.NOTEQUAL, text: 'Not equal' }).appendTo(inp);
-		$('<option />', { value: df.GEQUAL, text: 'Greater / equal' }).appendTo(inp);
-		$('<option />', { value: df.Always, text: 'Always' }).appendTo(inp);
-		 
-		inp.change(function() 
-		{
-			self.state.depth_func = parseInt(inp.val());
-			self.state_changed(inp);
-			self.changed = true;
-		});
-		
-		return inp;
-	};
+E2.p.prototype.reset = function()
+{
+	this.updated = true;
+};
+
+E2.p.prototype.create_ui = function()
+{
+	var df = Material.depth_func;
+	var inp = $('<select />', { selectedIndex: 4 });
 	
-	this.update_state = function(delta_t)
+	$('<option />', { value: df.NEVER, text: 'Never' }).appendTo(inp);
+	$('<option />', { value: df.LESS, text: 'Less' }).appendTo(inp);
+	$('<option />', { value: df.EQUAL, text: 'Equal' }).appendTo(inp);
+	$('<option />', { value: df.LEQUAL, text: 'Less / equal' }).appendTo(inp);
+	$('<option />', { value: df.GREATER, text: 'Greater' }).appendTo(inp);
+	$('<option />', { value: df.NOTEQUAL, text: 'Not equal' }).appendTo(inp);
+	$('<option />', { value: df.GEQUAL, text: 'Greater / equal' }).appendTo(inp);
+	$('<option />', { value: df.ALWAYS, text: 'Always' }).appendTo(inp);
+	 
+	inp.change(function(self) { return function() 
 	{
-		if(self.changed)
-		{
-			self.changed = false;
-			self.updated = true;
-		}
-	};
+		self.state.depth_func = parseInt(inp.val());
+		self.state_changed(inp);
+		self.changed = true;
+	}}(this));
 	
-	this.update_output = function(slot)
+	return inp;
+};
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	if(this.changed)
 	{
-		return self.state.depth_func;
-	};
-	
-	this.state_changed = function(ui)
-	{
-		if(ui)
-			ui.val('' + self.state.depth_func);
-	};
+		this.changed = false;
+		this.updated = true;
+	}
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.state.depth_func;
+};
+
+E2.p.prototype.state_changed = function(ui)
+{
+	if(ui)
+		ui.val('' + this.state.depth_func);
 };
