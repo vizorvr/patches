e2a51fcd2b8abff0a7eaca1273432340d3939ade
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-28 00:04:30 +0100

    Refactored the oscillator to unit plugin to use prototype.

--- plugins/convert_oscilator_unit_modulator.plugin.js
+++ plugins/convert_oscilator_unit_modulator.plugin.js
@@ -1,24 +1,29 @@
-E2.plugins["convert_oscilator_unit_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["convert_oscilator_unit_modulator"] = function(core, node)
+{
 	this.desc = 'Rescales and offsets a number in the range -1;1 to the range 0;1.';
-	this.input_slots = [ { name: 'value', dt: core.datatypes.FLOAT, desc: 'The input value.', lo: -1, hi: 1, def: 0 } ];	
-	this.output_slots = [ { name: 'value', dt: core.datatypes.FLOAT, desc: 'The output value: (I + 1) / 2', lo: 0, hi: 1, def: 0 } ];
 	
-	this.reset = function()
-	{
-		self.value = 0.0;
-	};
+	this.input_slots = [
+		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'The input value.', lo: -1, hi: 1, def: 0 }
+	];
 	
-	this.update_input = function(slot, data)
-	{
-		var v = (data + 1.0) * 0.5;
-		
-		self.value = v < 0.0 ? 0.0 : v > 1.0 ? 1.0 : v;
-	};
+	this.output_slots = [
+		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'The output value: (I + 1) / 2', lo: 0, hi: 1, def: 0 }
+	];
+};
+
+E2.p.prototype.reset = function()
+{
+	this.value = 0.0;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	var v = (data + 1.0) * 0.5;
 	
-	this.update_output = function(slot)
-	{
-		return self.value;
-	};
+	this.value = v < 0.0 ? 0.0 : v > 1.0 ? 1.0 : v;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.value;
 };
