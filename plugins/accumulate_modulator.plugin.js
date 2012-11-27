e2f93e3510ed7ef897907a0d5f4bbabf0df6e7a9
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 14:50:51 +0100

    Refactored the add modulator plugin to use prototype.

--- plugins/accumulate_modulator.plugin.js
+++ plugins/accumulate_modulator.plugin.js
@@ -1,23 +1,30 @@
-E2.plugins["accumulate_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["accumulate_modulator"] = function(core, node)
+{
 	this.desc = 'Every input <b>value</b> is accumulated in an internal buffer (which resets to zero on playback stop).';
-	this.input_slots = [ { name: 'value', dt: core.datatypes.FLOAT, desc: 'A small value to be accumulated in an internal buffer.', def: 0 } ];
-	this.output_slots = [ { name: 'value', dt: core.datatypes.FLOAT, desc: 'The current value of the accumulation buffer.', def: 0 } ];
+	
+	this.input_slots = [
+		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'A small value to be accumulated in an internal buffer.', def: 0 }
+	];
+	
+	this.output_slots = [
+		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'The current value of the accumulation buffer.', def: 0 }
+	];
+	
 	this.state = { value: 0.0 };
-	
-	this.reset = function()
-	{
-		self.updated = true;
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		self.state.value += data;
-	};	
-
-	this.update_output = function(slot)
-	{
-		return self.state.value;
-	};	
 };
+
+E2.p.prototype.reset = function()
+{
+	this.updated = true;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.state.value += data;
+};	
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.state.value;
+};	
+
