f65178dcc5ad606306ea4f8509de81f3cb2f1d55
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 14:57:42 +0100

    Refactored the audio loader plugin to use prototype. Added note about fixing silent load errors later.

--- plugins/audio_player.plugin.js
+++ plugins/audio_player.plugin.js
@@ -1,7 +1,7 @@
-E2.plugins["audio_player"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["audio_player"] = function(core, node)
+{
 	this.desc = 'Play an audio stream. Playback loops.';
+	
 	this.input_slots = [ 
 		{ name: 'audio', dt: core.datatypes.AUDIO, desc: 'The audio stream to play.' },
 		{ name: 'play', dt: core.datatypes.BOOL, desc: 'Send true to start playback and false to stop.' }
@@ -12,38 +12,38 @@ E2.plugins["audio_player"] = function(core, node) {
 	this.audio = null;
 	this.playing = false;
 	this.should_play = false;
-		
-	this.reset = function()
-	{
-		if(self.audio && self.playing)
-		{
-			self.playing = self.should_play = false;
-			self.audio.pause();
-			self.currentTime = 0;
-		}
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		if(slot.index === 0)
-			self.audio = data;
-		else
-			self.should_play = data;
-	};
+};
 
-	this.update_state = function(delta_t)
+E2.p.prototype.reset = function()
+{
+	if(this.audio && this.playing)
 	{
-		if(self.playing !== self.should_play)
+		this.playing = this.should_play = false;
+		this.audio.pause();
+		this.audio.currentTime = 0;
+	}
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	if(slot.index === 0)
+		this.audio = data;
+	else
+		this.should_play = data;
+};
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	if(this.playing !== this.should_play)
+	{
+		if(this.audio)
 		{
-			if(self.audio)
-			{
-				if(self.should_play)
-					self.audio.play();
-				else
-					self.audio.pause();			
-			}
-			
-			self.playing = self.should_play;
+			if(this.should_play)
+				this.audio.play();
+			else
+				this.audio.pause();			
 		}
-	};
+		
+		this.playing = this.should_play;
+	}
 };
