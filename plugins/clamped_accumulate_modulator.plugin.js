6c93311a4e21fa119982b5376aa3bab233c4d654
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 14:59:51 +0100

    Refactored the clamped accumulate plugin to use prototype.

--- plugins/clamped_accumulate_modulator.plugin.js
+++ plugins/clamped_accumulate_modulator.plugin.js
@@ -1,49 +1,51 @@
-E2.plugins["clamped_accumulate_modulator"] = function(core, node) {
-	var self = this;
+E2.p = E2.plugins["clamped_accumulate_modulator"] = function(core, node)
+{
+	this.desc = 'Every input <b>value</b> is accumulated in an internal buffer. The buffer value is not permitted to be smaller then <b>min</b> or larger than <b>max</b>.';
 	
-	this.desc = 'Every input <b>value</b> is accumulated in an internal buffer (which resets to zero on playback stop). The buffer value is not permitted to be smaller then <b>min</b> or larger than <b>max</b>.';
 	this.input_slots = 
 	[
 		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'A small value to be accumulated in an internal buffer.', def: 0 },
 		{ name: 'min', dt: core.datatypes.FLOAT, desc: 'Minimum internal buffer value.', def: 0 },
 		{ name: 'max', dt: core.datatypes.FLOAT, desc: 'Maximum internal buffer value.', def: 1 }
 	];
-	this.output_slots = [ { name: 'value', dt: core.datatypes.FLOAT, desc: 'The current value of the accumulation buffer.', def: 0 } ];
+	
+	this.output_slots = [
+		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'The current value of the accumulation buffer.', def: 0 }
+	];
+	
 	this.state = { value: 0.0 };
-	
-	this.reset = function()
-	{
-		self.updated = true;
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		if(slot.index === 0)
-		{
-			self.value = data;
+};
 
-			self.state.value += self.value;
+E2.p.prototype.reset = function()
+{
+	this.updated = true;
+};
 
-			self.state.value = self.state.value < self.lo ? self.lo : self.state.value > self.hi ? self.hi : self.state.value;
-		}
-		if(slot.index === 1)
-			self.lo = data;
-		if(slot.index === 2)
-			self.hi = data;
-	};
-	
-	this.update_output = function(slot)
+E2.p.prototype.update_input = function(slot, data)
+{
+	if(slot.index === 0)
 	{
-		return self.state.value;
-	};	
+		this.value = data;
+		this.state.value += this.value;
+		this.state.value = this.state.value < this.lo ? this.lo : this.state.value > this.hi ? this.hi : this.state.value;
+	}
+	if(slot.index === 1)
+		this.lo = data;
+	if(slot.index === 2)
+		this.hi = data;
+};
 
-	this.state_changed = function(ui)
+E2.p.prototype.update_output = function(slot)
+{
+	return this.state.value;
+};	
+
+E2.p.prototype.state_changed = function(ui)
+{
+	if(!ui)
 	{
-		if(!ui)
-		{
-			self.value = 0.0;
-			self.lo = 0.0;
-			self.hi = 1.0;
-		}
-	};
+		this.value = 0.0;
+		this.lo = 0.0;
+		this.hi = 1.0;
+	}
 };
