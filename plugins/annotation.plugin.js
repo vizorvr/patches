9e8bd44c31a43ab02470a2ddfc479ebf5b75f13c
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 14:55:51 +0100

    Refactored the annotation plugin the use prototype. +Minor deserialization optimization.

--- plugins/annotation.plugin.js
+++ plugins/annotation.plugin.js
@@ -1,50 +1,54 @@
-E2.plugins["annotation"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["annotation"] = function(core, node)
+{
 	this.desc = 'Add textural hints to the graph.';
+	
 	this.input_slots = [];
+	
 	this.output_slots = [];
+	
 	this.state = { text: '', width: 0, height: 0 };
+};
+
+E2.p.prototype.create_ui = function()
+{
+	var inp = $('<textarea placeholder="Type text here" />');
 	
-	this.create_ui = function()
-	{
-		var inp = $('<textarea placeholder="Type text here" />');
-		
-		inp.css('font-size', '8pt');
-		inp.css('border', '1px solid #999');
-		inp.css('margin', '0px');
-		inp.css('margin-top', '2px');
-		inp.css('padding', '2px');
-		
-		inp.bind('blur', function()
-		{
-			self.state.text = $(this).val();
-		});
-		
-		// Chrome doesn't handle resize properly for anything but the window object,
-		// so we store the potentially altered size of the textarea on mouseup.
-		inp.mouseup(function()
-		{
-			var ta = $(this);
-			
-			self.state.width = ta.width();
-			self.state.height = ta.height();
-		});
-		
-		return inp;
-	};
+	inp.css('font-size', '8pt');
+	inp.css('border', '1px solid #999');
+	inp.css('margin', '0px');
+	inp.css('margin-top', '2px');
+	inp.css('padding', '2px');
 	
-	this.state_changed = function(ui)
+	inp.bind('blur', function(self) { return function()
 	{
-		if(ui && self.state.text !== '')
-		{
-			ui.val(self.state.text);
-			
-			if(self.state.width > 0)
-				ui.css('width', self.state.width);
-			
-			if(self.state.height > 0)
-				ui.css('height', self.state.height);
-		}
-	};
+		self.state.text = $(this).val();
+	}}(this));
+	
+	// Chrome doesn't handle resize properly for anything but the window object,
+	// so we store the potentially altered size of the textarea on mouseup.
+	inp.mouseup(function(self) { return function()
+	{
+		var ta = $(this);
+		
+		self.state.width = ta.width();
+		self.state.height = ta.height();
+	}}(this));
+	
+	return inp;
+};
+
+E2.p.prototype.state_changed = function(ui)
+{
+	var s = this.state;
+	
+	if(ui && s.text !== '')
+	{
+		ui.val(s.text);
+		
+		if(s.width > 0)
+			ui.css('width', s.width);
+		
+		if(s.height > 0)
+			ui.css('height', s.height);
+	}
 };
