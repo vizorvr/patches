9ea68082a519e4c0c2f270a7559aab38fd6abe0e
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 19:38:10 +0100

    Refactored the convert bool to float plugin to use prototype.

--- plugins/convert_bool_float_modulator.plugin.js
+++ plugins/convert_bool_float_modulator.plugin.js
@@ -1,7 +1,7 @@
-E2.plugins["convert_bool_float_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["convert_bool_float_modulator"] = function(core, node)
+{
 	this.desc = 'Convert a bool to a float. True emits 1 and false 0.';
+	
 	this.input_slots = [
 		{ name: 'bool', dt: core.datatypes.BOOL, desc: 'Input boolean to be converted.', def: 'False' } 
 	];
@@ -9,19 +9,19 @@ E2.plugins["convert_bool_float_modulator"] = function(core, node) {
 	this.output_slots = [ 
 		 { name: 'value', dt: core.datatypes.FLOAT, desc: 'Emits 0 when the input is false and 1 otherwise.', lo: 0, hi: 1, def: 0 }
 	];
-	
-	this.reset = function()
-	{
-		self.value = 0.0;
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		self.value = data ? 1.0 : 0.0;
-	};
-	
-	this.update_output = function(slot)
-	{
-		return self.value;
-	};
+};
+
+E2.p.prototype.reset = function()
+{
+	this.value = 0.0;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.value = data ? 1.0 : 0.0;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.value;
 };
