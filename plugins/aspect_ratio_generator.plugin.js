8725e5dd7813e0b27790f2e7c36754255880c199
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 14:56:59 +0100

    Refactored the aspect ratio generator plugin to use prototype. Fixed bug where the plugin would not re-emit its value when playback is restarted after having been stopped.

--- plugins/aspect_ratio_generator.plugin.js
+++ plugins/aspect_ratio_generator.plugin.js
@@ -1,14 +1,22 @@
-E2.plugins["aspect_ratio_generator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["aspect_ratio_generator"] = function(core, node)
+{
 	this.desc = 'Emits current renderer aspect ratio (width / height).';
+	
 	this.input_slots = [];
+	
 	this.output_slots = [ { name: 'aspect', dt: core.datatypes.FLOAT, desc: 'The current renderer aspect ratio.' } ];
 	
-	this.update_output = function(slot)
-	{
-		var c = core.renderer.canvas;
+	this.canvas = core.renderer.canvas;
+};
 
-		return c.width() / c.height();
-	};
+E2.p.prototype.reset = function()
+{
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	var c = this.canvas;
+
+	// TODO: We've *got* to get rid of this. It locks up the UI thread badly.
+	return c.width() / c.height();
 };
