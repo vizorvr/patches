e576c09e791a9af9e7c7138e16626d2b946ca321
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-28 00:06:11 +0100

    Refactored the cube mesh plugin to use prototype.

--- plugins/cube_mesh_generator.plugin.js
+++ plugins/cube_mesh_generator.plugin.js
@@ -1,154 +1,160 @@
-E2.plugins["cube_mesh_generator"] = function(core, node) {
-	var self = this;
-	var gl = core.renderer.context;
-	
+E2.p = E2.plugins["cube_mesh_generator"] = function(core, node)
+{
 	this.desc = 'Create a cube mesh of unit size with normals and one set of UV-cordinates.';
+	
 	this.input_slots = [];
-	this.output_slots = [ { name: 'mesh', dt: core.datatypes.MESH, desc: 'Cube mesh.' } ];
+	
+	this.output_slots = [
+		{ name: 'mesh', dt: core.datatypes.MESH, desc: 'Cube mesh.' }
+	];
+	
+	this.gl = core.renderer.context;
 	this.mesh = null;
-	
-	this.reset = function()
+};
+
+E2.p.prototype.reset = function()
+{
+	this.updated = true;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.mesh;
+};
+
+E2.p.prototype.state_changed = function(ui)
+{
+	if(!ui)
 	{
-		self.updated = true;
-	};
-	
-	this.update_output = function(slot)
-	{
-		return self.mesh;
-	};
-	
-	this.state_changed = function(ui)
-	{
-		if(!ui)
-		{
-			msg('Cube: Generating mesh.');
-			self.mesh = new Mesh(gl, gl.TRIANGLES);
+		var gl = this.gl;
+		
+		msg('Cube: Generating mesh.');
+		this.mesh = new Mesh(gl, gl.TRIANGLES);
+		
+		var verts = this.mesh.vertex_buffers['VERTEX'] = new VertexBuffer(gl, VertexBuffer.vertex_type.VERTEX);
+		
+		verts.bind_data([
+			// Front face
+			-1.0, -1.0,  1.0,
+			1.0, -1.0,  1.0,
+			1.0,  1.0,  1.0,
+			-1.0,  1.0,  1.0,
+
+			// Back face
+			-1.0, -1.0, -1.0,
+			-1.0,  1.0, -1.0,
+			1.0,  1.0, -1.0,
+			1.0, -1.0, -1.0,
+
+			// Top face
+			-1.0,  1.0, -1.0,
+			-1.0,  1.0,  1.0,
+			1.0,  1.0,  1.0,
+			1.0,  1.0, -1.0,
+
+			// Bottom face
+			-1.0, -1.0, -1.0,
+			1.0, -1.0, -1.0,
+			1.0, -1.0,  1.0,
+			-1.0, -1.0,  1.0,
+
+			// Right face
+			1.0, -1.0, -1.0,
+			1.0,  1.0, -1.0,
+			1.0,  1.0,  1.0,
+			1.0, -1.0,  1.0,
+
+			// Left face
+			-1.0, -1.0, -1.0,
+			-1.0, -1.0,  1.0,
+			-1.0,  1.0,  1.0,
+			-1.0,  1.0, -1.0
+		]);
 			
-			var verts = self.mesh.vertex_buffers['VERTEX'] = new VertexBuffer(gl, VertexBuffer.vertex_type.VERTEX);
-			
-			verts.bind_data([
-				// Front face
-				-1.0, -1.0,  1.0,
-				1.0, -1.0,  1.0,
-				1.0,  1.0,  1.0,
-				-1.0,  1.0,  1.0,
+		var norms = this.mesh.vertex_buffers['NORMAL'] = new VertexBuffer(gl, VertexBuffer.vertex_type.NORMAL);
+		
+		norms.bind_data([
+			// Front
+			0.0,  0.0,  1.0,
+			0.0,  0.0,  1.0,
+			0.0,  0.0,  1.0,
+			0.0,  0.0,  1.0,
 
-				// Back face
-				-1.0, -1.0, -1.0,
-				-1.0,  1.0, -1.0,
-				1.0,  1.0, -1.0,
-				1.0, -1.0, -1.0,
+			// Back
+			0.0,  0.0, -1.0,
+			0.0,  0.0, -1.0,
+			0.0,  0.0, -1.0,
+			0.0,  0.0, -1.0,
 
-				// Top face
-				-1.0,  1.0, -1.0,
-				-1.0,  1.0,  1.0,
-				1.0,  1.0,  1.0,
-				1.0,  1.0, -1.0,
+			// Top
+			0.0,  1.0,  0.0,
+			0.0,  1.0,  0.0,
+			0.0,  1.0,  0.0,
+			0.0,  1.0,  0.0,
 
-				// Bottom face
-				-1.0, -1.0, -1.0,
-				1.0, -1.0, -1.0,
-				1.0, -1.0,  1.0,
-				-1.0, -1.0,  1.0,
+			// Bottom
+			0.0, -1.0,  0.0,
+			0.0, -1.0,  0.0,
+			0.0, -1.0,  0.0,
+			0.0, -1.0,  0.0,
 
-				// Right face
-				1.0, -1.0, -1.0,
-				1.0,  1.0, -1.0,
-				1.0,  1.0,  1.0,
-				1.0, -1.0,  1.0,
+			// Right
+			1.0,  0.0,  0.0,
+			1.0,  0.0,  0.0,
+			1.0,  0.0,  0.0,
+			1.0,  0.0,  0.0,
 
-				// Left face
-				-1.0, -1.0, -1.0,
-				-1.0, -1.0,  1.0,
-				-1.0,  1.0,  1.0,
-				-1.0,  1.0, -1.0
-			]);
-				
-			var norms = self.mesh.vertex_buffers['NORMAL'] = new VertexBuffer(gl, VertexBuffer.vertex_type.NORMAL);
-			
-			norms.bind_data([
-				// Front
-				0.0,  0.0,  1.0,
-				0.0,  0.0,  1.0,
-				0.0,  0.0,  1.0,
-				0.0,  0.0,  1.0,
+			// Left
+			-1.0,  0.0,  0.0,
+			-1.0,  0.0,  0.0,
+			-1.0,  0.0,  0.0,
+			-1.0,  0.0,  0.0
+		]);
 
-				// Back
-				0.0,  0.0, -1.0,
-				0.0,  0.0, -1.0,
-				0.0,  0.0, -1.0,
-				0.0,  0.0, -1.0,
+		var uv0 = this.mesh.vertex_buffers['UV0'] = new VertexBuffer(gl, VertexBuffer.vertex_type.UV0);
+		
+		uv0.bind_data([
+			// Front
+			0.0,  0.0,
+			1.0,  0.0,
+			1.0,  1.0,
+			0.0,  1.0,
+			// Back
+			0.0,  0.0,
+			1.0,  0.0,
+			1.0,  1.0,
+			0.0,  1.0,
+			// Top
+			0.0,  0.0,
+			1.0,  0.0,
+			1.0,  1.0,
+			0.0,  1.0,
+			// Bottom
+			0.0,  0.0,
+			1.0,  0.0,
+			1.0,  1.0,
+			0.0,  1.0,
+			// Right
+			0.0,  0.0,
+			1.0,  0.0,
+			1.0,  1.0,
+			0.0,  1.0,
+			// Left
+			0.0,  0.0,
+			1.0,  0.0,
+			1.0,  1.0,
+			0.0,  1.0
+		]);
 
-				// Top
-				0.0,  1.0,  0.0,
-				0.0,  1.0,  0.0,
-				0.0,  1.0,  0.0,
-				0.0,  1.0,  0.0,
-
-				// Bottom
-				0.0, -1.0,  0.0,
-				0.0, -1.0,  0.0,
-				0.0, -1.0,  0.0,
-				0.0, -1.0,  0.0,
-
-				// Right
-				1.0,  0.0,  0.0,
-				1.0,  0.0,  0.0,
-				1.0,  0.0,  0.0,
-				1.0,  0.0,  0.0,
-
-				// Left
-				-1.0,  0.0,  0.0,
-				-1.0,  0.0,  0.0,
-				-1.0,  0.0,  0.0,
-				-1.0,  0.0,  0.0
-			]);
-
-  			var uv0 = self.mesh.vertex_buffers['UV0'] = new VertexBuffer(gl, VertexBuffer.vertex_type.UV0);
-			
-			uv0.bind_data([
-				// Front
-				0.0,  0.0,
-				1.0,  0.0,
-				1.0,  1.0,
-				0.0,  1.0,
-				// Back
-				0.0,  0.0,
-				1.0,  0.0,
-				1.0,  1.0,
-				0.0,  1.0,
-				// Top
-				0.0,  0.0,
-				1.0,  0.0,
-				1.0,  1.0,
-				0.0,  1.0,
-				// Bottom
-				0.0,  0.0,
-				1.0,  0.0,
-				1.0,  1.0,
-				0.0,  1.0,
-				// Right
-				0.0,  0.0,
-				1.0,  0.0,
-				1.0,  1.0,
-				0.0,  1.0,
-				// Left
-				0.0,  0.0,
-				1.0,  0.0,
-				1.0,  1.0,
-				0.0,  1.0
-			]);
-  
-			var indices = self.mesh.index_buffer = new IndexBuffer(gl);
-			
-			indices.bind_data([
-				0,  1,  2,      0,  2,  3,    // front
-				4,  5,  6,      4,  6,  7,    // back
-				8,  9,  10,     8,  10, 11,   // top
-				12, 13, 14,     12, 14, 15,   // bottom
-				16, 17, 18,     16, 18, 19,   // right
-				20, 21, 22,     20, 22, 23    // left
-			]);
-		}
-	};
+		var indices = this.mesh.index_buffer = new IndexBuffer(gl);
+		
+		indices.bind_data([
+			0,  1,  2,      0,  2,  3,    // front
+			4,  5,  6,      4,  6,  7,    // back
+			8,  9,  10,     8,  10, 11,   // top
+			12, 13, 14,     12, 14, 15,   // bottom
+			16, 17, 18,     16, 18, 19,   // right
+			20, 21, 22,     20, 22, 23    // left
+		]);
+	}
 };
