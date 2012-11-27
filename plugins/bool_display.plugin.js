2900672cdce6b3ea889e17228be1b17ca65ce2f8
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-27 14:58:23 +0100

    Refactored the bool display plugin to use prototype.

--- plugins/bool_display.plugin.js
+++ plugins/bool_display.plugin.js
@@ -1,41 +1,43 @@
-E2.plugins["bool_display"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["bool_display"] = function(core, node)
+{
 	this.desc = 'Displays the text \'True\' or \'False\' depending on the state of the supplied bool or \'-\', if no data is being received.';
+	
 	this.input_slots = [ 
 		{ name: 'bool', dt: core.datatypes.BOOL, desc: 'The input boolean to display.' }
 	];
 	
 	this.output_slots = [];
 	
-	this.reset = function()
-	{
-		self.update_value(null);
-	}
-	
-	this.create_ui = function()
-	{
-		self.label = make('div');
-		self.label.css('text-align', 'right'); 
-		self.update_value(null);
-		
-		return self.label;
-	};
-	
-	this.connection_changed = function(on, conn, slot)
-	{
-		if(!on)
-			self.update_value(null);
-	};
+	this.label = null;
+};
 
-	this.update_input = function(slot, data)
-	{
-		self.update_value(data);
-	};
+E2.p.prototype.reset = function()
+{
+	this.update_value(null);
+}
+
+E2.p.prototype.create_ui = function()
+{
+	this.label = make('div');
+	this.label.css('text-align', 'right'); 
+	this.update_value(null);
 	
-	this.update_value = function(value)
-	{	
-		if(self.label)
-			self.label.html(value === null ? '-' : value ? 'True' : 'False');
-	};
+	return this.label;
+};
+
+E2.p.prototype.connection_changed = function(on, conn, slot)
+{
+	if(!on)
+		this.update_value(null);
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.update_value(data);
+};
+
+E2.p.prototype.update_value = function(value)
+{	
+	if(this.label)
+		this.label.html(value === null ? '-' : value ? 'True' : 'False');
 };
