4253a08838aeddc018be308d289ebe79d55e803a
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-28 00:08:02 +0100

    Refactored the divide plugin to use prototype.

--- plugins/divide_modulator.plugin.js
+++ plugins/divide_modulator.plugin.js
@@ -1,36 +1,38 @@
-E2.plugins["divide_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["divide_modulator"] = function(core, node)
+{
 	this.desc = 'Divides <b>value</b> by <b>scalar</b> and emits the result.';
+	
 	this.input_slots = [ 
 		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'Input value to be divided.', def: 0 },
 		{ name: 'scalar', dt: core.datatypes.FLOAT, desc: 'Factor to divide <b>value</b> by.', lo: '>0', def: 1 } 
 	];
 	
-	this.output_slots = [ { name: 'result', dt: core.datatypes.FLOAT, desc: 'Emits <b>value</b> / <b>scalar</b>.', def: 0 } ];
-	
-	this.reset = function()
-	{
-		self.value = 0.0;
-		self.scalar = 1.0;
-		self.result = 0.0;
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		if(slot.index === 0)
-			self.value = data;
-		else
-			self.scalar = data < 0.0000001 ? 1.0 : data;
-	};	
+	this.output_slots = [
+		{ name: 'result', dt: core.datatypes.FLOAT, desc: 'Emits <b>value</b> / <b>scalar</b>.', def: 0 }
+	];
+};
 
-	this.update_state = function(delta_t)
-	{
-		self.result = self.value / self.scalar;
-	};
-	
-	this.update_output = function(slot)
-	{
-		return self.result;
-	};	
+E2.p.prototype.reset = function()
+{
+	this.value = 0.0;
+	this.scalar = 1.0;
+	this.result = 0.0;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	if(slot.index === 0)
+		this.value = data;
+	else
+		this.scalar = data < 0.0000001 ? 1.0 : data;
+};	
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	this.result = this.value / this.scalar;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.result;
 };
