1adee782c4ff9dda98f5cfc18d4848a69854b438
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-28 00:03:36 +0100

    Refactored the HSLA -> color plugin to use prototype.

--- plugins/convert_hsla_color_modulator.plugin.js
+++ plugins/convert_hsla_color_modulator.plugin.js
@@ -1,7 +1,7 @@
-E2.plugins["convert_hsla_color_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["convert_hsla_color_modulator"] = function(core, node)
+{
 	this.desc = 'Create an RGBA color from hue, saturation and luminosity and alpha.';
+	
 	this.input_slots = [
 		 { name: 'hue', dt: core.datatypes.FLOAT, desc: 'The hue of the output color.', lo: 0, hi: 1, def: 1 },
 		 { name: 'saturation', dt: core.datatypes.FLOAT, desc: 'The saturation (color intensity) of the output color.', lo: 0, hi: 1, def: 1 },
@@ -12,59 +12,59 @@ E2.plugins["convert_hsla_color_modulator"] = function(core, node) {
 	this.output_slots = [ 
 		{ name: 'color', dt: core.datatypes.COLOR, desc: 'The corresponding RGBA color.', def: 'White' } 
 	];
-	
-	this.reset = function()
-	{
-		self.hsla = [1.0, 1.0, 1.0, 1.0];
-		self.color = new Color(1.0, 1.0, 1.0, 1.0);
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		self.hsla[slot.index] = data < 0.0 ? 0.0 : data > 1.0 ? 1.0 : data;
-	};
-	
-	this.update_state = function(delta_t)
-	{
-		var s = self.hsla[1];
-		var l = self.hsla[2];
-		var sc = self.color.rgba;
-		
-		sc[3] = self.hsla[3];
+};
 
-		if(s === 0.0)
-		{
-			sc[0] = sc[1] = sc[2] = l;
-			return;
-		}
-		
-		var h = self.hsla[0];
-		var t2 = ((l <= 0.5) ? l * (1.0 + s) : l + s - (l * s));
-		var t1 = 2.0 * l - t2;		
-		var t3 = [h + 1.0 / 3.0, h, h - 1.0 / 3.0];
-		
-		for(var i = 0; i < 3; i++)
-		{
-			if(t3[i] < 0.0)
-				t3[i] += 1.0;
-			if(t3[i] > 1.0)
-				t3[i] -= 1.0;
-			
-			var t3v = t3[i];
-						
-			if(6.0 * t3v < 1.0)
-				sc[i] = t1 + (t2-t1) * t3v * 6.0;
-			else if(2.0 * t3v < 1.0)
-				sc[i] = t2;
-			else if(3.0 * t3v < 2.0)
-				sc[i] = (t1 + (t2 - t1) * ((2.0 / 3.0) - t3v) * 6.0);
-			else
-				sc[i] = t1;
-		}
-       	};
+E2.p.prototype.reset = function()
+{
+	this.hsla = [1.0, 1.0, 1.0, 1.0];
+	this.color = new Color(1.0, 1.0, 1.0, 1.0);
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.hsla[slot.index] = data < 0.0 ? 0.0 : data > 1.0 ? 1.0 : data;
+};
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	var s = this.hsla[1];
+	var l = this.hsla[2];
+	var sc = this.color.rgba;
 	
-	this.update_output = function(slot)
+	sc[3] = this.hsla[3];
+
+	if(s === 0.0)
 	{
-		return self.color;
-	};
+		sc[0] = sc[1] = sc[2] = l;
+		return;
+	}
+	
+	var h = this.hsla[0];
+	var t2 = ((l <= 0.5) ? l * (1.0 + s) : l + s - (l * s));
+	var t1 = 2.0 * l - t2;		
+	var t3 = [h + 1.0 / 3.0, h, h - 1.0 / 3.0];
+	
+	for(var i = 0; i < 3; i++)
+	{
+		if(t3[i] < 0.0)
+			t3[i] += 1.0;
+		if(t3[i] > 1.0)
+			t3[i] -= 1.0;
+		
+		var t3v = t3[i];
+					
+		if(6.0 * t3v < 1.0)
+			sc[i] = t1 + (t2-t1) * t3v * 6.0;
+		else if(2.0 * t3v < 1.0)
+			sc[i] = t2;
+		else if(3.0 * t3v < 2.0)
+			sc[i] = (t1 + (t2 - t1) * ((2.0 / 3.0) - t3v) * 6.0);
+		else
+			sc[i] = t1;
+	}
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.color;
 };
