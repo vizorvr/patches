b678fafc8b059a1e3d8f324638ae579809a95653
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-28 21:13:12 +0100

    Refactored the float display plugin to use prototype.

--- plugins/float_display.plugin.js
+++ plugins/float_display.plugin.js
@@ -1,41 +1,41 @@
-E2.plugins["float_display"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["float_display"] = function(core, node)
+{
 	this.desc = 'Display the supplied float value on the plugin surface.';
+	
 	this.input_slots = [ 
 		{ name: 'float', dt: core.datatypes.FLOAT, desc: 'Input value to be displayed.' }
 	];
 	
 	this.output_slots = [];
+};
 
-	this.reset = function()
-	{
-		self.update_value(null);
-	}
-	
-	this.create_ui = function()
-	{
-		self.label = make('div');
-		self.label.css('text-align', 'right'); 
-		self.update_value(null);
-		
-		return self.label;
-	};
-	
-	this.connection_changed = function(on, conn, slot)
-	{
-		if(!on)
-			self.update_value(null);
-	};
+E2.p.prototype.reset = function()
+{
+	this.update_value(null);
+}
 
-	this.update_input = function(slot, data)
-	{
-		self.update_value(data);
-	};
+E2.p.prototype.create_ui = function()
+{
+	this.label = make('div');
+	this.label.css('text-align', 'right'); 
+	this.update_value(null);
 	
-	this.update_value = function(value)
-	{
-		if(self.label)
-			self.label.html(value === null ? '-' : value.toFixed(2));
-	};
+	return this.label;
+};
+
+E2.p.prototype.connection_changed = function(on, conn, slot)
+{
+	if(!on)
+		this.update_value(null);
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.update_value(data);
+};
+
+E2.p.prototype.update_value = function(value)
+{
+	if(this.label)
+		this.label.html(value === null ? '-' : value.toFixed(2));
 };
