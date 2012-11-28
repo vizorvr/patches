cfcbe80354f3ce1ea73618e0f4f955a9a804f0a5
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-28 14:26:52 +0100

    Optimization of the aspect ratio plugin: Removed dependence of jQuery to improve speed of aspect ratio calculation and minimize UI thread lockups.

--- plugins/aspect_ratio_generator.plugin.js
+++ plugins/aspect_ratio_generator.plugin.js
@@ -6,7 +6,7 @@ E2.p = E2.plugins["aspect_ratio_generator"] = function(core, node)
 	
 	this.output_slots = [ { name: 'aspect', dt: core.datatypes.FLOAT, desc: 'The current renderer aspect ratio.' } ];
 	
-	this.canvas = core.renderer.canvas;
+	this.canvas = core.renderer.canvas[0];
 };
 
 E2.p.prototype.reset = function()
@@ -17,6 +17,5 @@ E2.p.prototype.update_output = function(slot)
 {
 	var c = this.canvas;
 
-	// TODO: We've *got* to get rid of this. It locks up the UI thread badly.
-	return c.width() / c.height();
+	return c.width / c.height;
 };
