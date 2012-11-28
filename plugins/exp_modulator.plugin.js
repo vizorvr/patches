21fcc45842a8dae25340d9f8af334c386d37c1e5
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-28 21:12:00 +0100

    Refactored the exp plugin to use prototype.

--- plugins/exp_modulator.plugin.js
+++ plugins/exp_modulator.plugin.js
@@ -1,26 +1,28 @@
-E2.plugins["exp_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["exp_modulator"] = function(core, node)
+{
 	this.desc = 'Exp(x).';
+	
 	this.input_slots = [ 
 		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'Input value.', def: 0 }
 	];
 	
-	this.output_slots = [ { name: 'result', dt: core.datatypes.FLOAT, desc: 'exp(<b>value</b>).', def: 0 } ];
-	
-	this.reset = function()
-	{
-		self.value = 0.0;
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		if(slot.index === 0)
-			self.value = Math.exp(data);
-	};	
-	
-	this.update_output = function(slot)
-	{
-		return self.value;
-	};	
+	this.output_slots = [
+		{ name: 'result', dt: core.datatypes.FLOAT, desc: 'exp(<b>value</b>).', def: 0 }
+	];
+};
+
+E2.p.prototype.reset = function()
+{
+	this.value = 0.0;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	if(slot.index === 0)
+		this.value = Math.exp(data);
+};	
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.value;
 };
