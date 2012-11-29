06dc75f295a256f464fe49aa8bdc9a8a33ad72ec
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-29 04:19:22 +0100

    Refactored if plugin to use prototype. Fixed bug where data would initially flow through the if plugin, even when it received false as a condition value, when a new connection was made to the value slot when playback was running while editing.

--- plugins/if_modulator.plugin.js
+++ plugins/if_modulator.plugin.js
@@ -1,36 +1,40 @@
-E2.plugins["if_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["if_modulator"] = function(core, node)
+{
 	this.desc = 'Allows <b>value</b> though if <b>condition</b> is true and inhibits futher data-flow otherwise.';
+	
 	this.input_slots = [ 
 		{ name: 'condition', dt: core.datatypes.BOOL, desc: 'Condition that, if true, allows <b>value</b> to be emitted.', def: 'False' },
 		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'Value to be emitted if <b>condition</b> is true.', def: 0 } 
 	];
 	
-	this.output_slots = [ { name: 'value', dt: core.datatypes.FLOAT, desc: 'Emits <b>value</b> if <b>condition</b> is true and nothing otherwise.' } ];
-	
-	this.reset = function()
-	{
-		self.condition = false;
-		self.value = 0.0;
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		if(slot.index === 0)
-			self.condition = data;
-		else
-			self.value = data;
-	};	
+	this.output_slots = [
+		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'Emits <b>value</b> if <b>condition</b> is true and nothing otherwise.' }
+	];
+};
 
-	this.update_state = function(delta_t)
-	{
-		if(!self.condition)
-			self.updated = false;
-	};
-	
-	this.update_output = function(slot)
-	{
-		return self.value;
-	};	
+E2.p.prototype.reset = function()
+{
+	this.condition = false;
+	this.out_value = this.value = 0.0;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	if(slot.index === 0)
+		this.condition = data;
+	else
+		this.value = data;
+};	
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	if(!this.condition)
+		this.updated = false;
+	else
+		this.out_value = this.value;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.out_value;
 };
