083480446dfc5d2b15fd33fc409e87863797618c
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-30 00:21:39 +0100

    Refactored the scene get mesh count plugin to use prototype.

--- plugins/scene_get_mesh_count.plugin.js
+++ plugins/scene_get_mesh_count.plugin.js
@@ -1,31 +1,36 @@
-E2.plugins["scene_get_mesh_count"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["scene_get_mesh_count"] = function(core, node)
+{
 	this.desc = 'Emits the number of meshes in the supplied <b>scene</b>. Note that the maximum index is one less than the mesh <b>count</b>.';
-	this.input_slots = [ { name: 'scene', dt: core.datatypes.SCENE, desc: 'The scene to obtain the mesh count from.' } ];
-	this.output_slots = [ { name: 'count', dt: core.datatypes.FLOAT, desc: 'The number of meshes in the supplied <b>scene</b>. Note that mesh indices are zero-index, so the highest mesh index in a scene is count - 1.', def: 0 } ];
 	
-	this.update_input = function(slot, data)
-	{
-		self.scene = data;
-	};	
-
-	this.update_state = function(delta_t)
-	{
-		self.count = self.scene ? self.scene.meshes.length : 0;
-	};
+	this.input_slots = [
+		{ name: 'scene', dt: core.datatypes.SCENE, desc: 'The scene to obtain the mesh count from.' }
+	];
 	
-	this.update_output = function(slot)
-	{
-		return self.count;
-	};	
+	this.output_slots = [
+		{ name: 'count', dt: core.datatypes.FLOAT, desc: 'The number of meshes in the supplied <b>scene</b>. Note that mesh indices are zero-index, so the highest mesh index in a scene is count - 1.', def: 0 }
+	];
+};
 
-	this.state_changed = function(ui)
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.scene = data;
+};	
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	this.count = this.scene ? this.scene.meshes.length : 0;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.count;
+};	
+
+E2.p.prototype.state_changed = function(ui)
+{
+	if(!ui)
 	{
-		if(!ui)
-		{
-			self.scene = null;
-			self.count = 0;
-		}
-	};
+		this.scene = null;
+		this.count = 0;
+	}
 };
