b9cf159a7b033e243199a262f91c031e5423becf
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-29 19:45:16 +0100

    Refactored the matrix display plugin to use prototype. Removed use of jQuery for updating call values to improve performance.

--- plugins/matrix_display.plugin.js
+++ plugins/matrix_display.plugin.js
@@ -1,68 +1,69 @@
-E2.plugins["matrix_display"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["matrix_display"] = function(core, node)
+{
 	this.desc = 'Displays the supplied matrix as a 4x4 grid of values.';
+	
 	this.input_slots = [ 
 		{ name: 'matrix', dt: core.datatypes.MATRIX, desc: 'The input matrix to be displayed.' }
 	];
+	
 	this.output_slots = [];
-		
-	this.reset = function()
-	{
-		self.cell_vals = ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'];
-		self.update_values();
-	};
+};
+
+E2.p.prototype.reset = function()
+{
+	this.cell_vals = ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'];
+	this.update_values();
+};
+
+E2.p.prototype.create_ui = function()
+{
+	var table = make('table');
 	
-	this.create_ui = function()
+	this.columns = [];
+	
+	for(var r = 0; r < 4; r++)
 	{
-		var table = make('table');
+		var row = make('tr');
 		
-		self.columns = [];
-		
-		for(var r = 0; r < 4; r++)
+		for(var c = 0; c < 4; c++)
 		{
-			var row = make('tr');
-			
-			for(var c = 0; c < 4; c++)
-			{
-				var col = make('td');
-			
-				col.html('-');
-				col.css('text-align', 'right');
-				col.css('padding-left', '10px');
-				self.columns.push(col);
-			
-				row.append(col);
-			}
-			
-			table.append(row);
-		}		
-
-		return table;
-	};
-	
-	this.connection_changed = function(on, conn, slot)
-	{
-		if(!on)
-			self.reset();
-	};
-
-	this.update_input = function(slot, data)
-	{
-		var ofs = 0;
+			var col = make('td');
 		
-		for(var i = 0; i < 16; i++)
-			self.cell_vals[i] = data[i].toFixed(2);
-	
-		self.update_values();
-	};
-	
-	this.update_values = function()
-	{
-		if(!self.columns)
-			return;
+			col.html('-');
+			col.css('text-align', 'right');
+			col.css('padding-left', '10px');
+			this.columns.push(col[0]);
 		
-		for(var i = 0; i < 16; i++)
-			self.columns[i].html(self.cell_vals[i]);
-	};
+			row.append(col);
+		}
+		
+		table.append(row);
+	}		
+
+	return table;
+};
+
+E2.p.prototype.connection_changed = function(on, conn, slot)
+{
+	if(!on)
+		this.reset();
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	var ofs = 0;
+	
+	for(var i = 0; i < 16; i++)
+		this.cell_vals[i] = data[i].toFixed(2);
+
+	this.update_values();
+};
+
+E2.p.prototype.update_values = function()
+{
+	if(!this.columns)
+		return;
+	
+	for(var i = 0; i < 16; i++)
+		this.columns[i].innerHTML = this.cell_vals[i];
 };
