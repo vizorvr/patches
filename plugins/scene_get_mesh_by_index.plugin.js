41a7c3871f365ed4dbef0a0ed2cba3bbe232aef5
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-30 00:21:21 +0100

    Refactored scene get mesh by index to use prototype. Updated the logic to ensure that a new mesh is only transmitted when the index changes.

--- plugins/scene_get_mesh_by_index.plugin.js
+++ plugins/scene_get_mesh_by_index.plugin.js
@@ -1,59 +1,78 @@
-E2.plugins["scene_get_mesh_by_index"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["scene_get_mesh_by_index"] = function(core, node)
+{
 	this.desc = 'Extract a single <b>mesh</b> from a <b>scene</b> by <b>index</b> so it can be rendered individually.';
+	
 	this.input_slots = [ 
 		{ name: 'scene', dt: core.datatypes.SCENE, desc: 'The scene to extract a mesh reference from.' },
 		{ name: 'index', dt: core.datatypes.FLOAT, desc: 'The desired mesh index.' }
 	];
 	
-	this.output_slots = [ { name: 'mesh', dt: core.datatypes.MESH, desc: 'The extracted mesh reference.' } ];
+	this.output_slots = [
+		{ name: 'mesh', dt: core.datatypes.MESH, desc: 'The extracted mesh reference.' }
+	];
+};
 
-	this.connection_changed = function(on, conn, slot)
-	{
-		if(!on && slot.type === E2.slot_type.input)
-		{
-			if(slot.index === 0)
-				self.scene = null;
-			else if(slot.index === 1)
-				self.index = 0;
-		}
-	};
-	
-	this.update_input = function(slot, data)
+E2.p.prototype.reset = function()
+{
+	this.changed = true;
+};
+
+E2.p.prototype.connection_changed = function(on, conn, slot)
+{
+	if(!on && slot.type === E2.slot_type.input)
 	{
 		if(slot.index === 0)
-			self.scene = data;
+			this.scene = null;
 		else if(slot.index === 1)
-			self.index = Math.floor(data < 0 ? -data : data);
-	};	
+			this.index = 0;
+	}
+};
 
-	this.update_state = function(delta_t)
-	{
-		if(self.scene)
-		{
-			var meshes = self.scene.meshes;
-			var count = meshes.length;
-			
-			if(count < 1)
-				return;
-			
-			self.mesh = meshes[self.index % count];
-		}
-	};
-	
-	this.update_output = function(slot)
-	{
-		return self.mesh;
-	};	
+E2.p.prototype.update_input = function(slot, data)
+{
+	if(slot.index === 0)
+		this.scene = data;
+	else if(slot.index === 1)
+		this.index = Math.floor(data < 0 ? -data : data);
+};	
 
-	this.state_changed = function(ui)
+E2.p.prototype.update_state = function(delta_t)
+{
+	if(this.scene)
 	{
-		if(!ui)
+		var meshes = this.scene.meshes;
+		var count = meshes.length;
+		
+		if(count < 1)
+			return;
+		
+		var m = meshes[this.index % count];
+		
+		if(m !== this.mesh)
 		{
-			self.scene = null;
-			self.index = 0;
-			self.mesh = null;
+			this.mesh = m;
+			return;
 		}
-	};
+	}
+
+	if(!this.changed)
+		this.updated = false;
+		
+	this.changed = false;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.mesh;
+};	
+
+E2.p.prototype.state_changed = function(ui)
+{
+	if(!ui)
+	{
+		this.scene = null;
+		this.index = 0;
+		this.mesh = null;
+		this.changed = true;
+	}
 };
