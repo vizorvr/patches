9f0b0b7096e913b19e701a616680269eaed953c2
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-29 16:24:04 +0100

    Refactored the invert matrix plugin to use prototype.

--- plugins/invert_matrix_modulator.plugin.js
+++ plugins/invert_matrix_modulator.plugin.js
@@ -1,40 +1,45 @@
-E2.plugins["invert_matrix_modulator"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["invert_matrix_modulator"] = function(core, node)
+{
 	this.desc = 'Emits the inverse of the supplied <b>matrix</b>.';
-	this.input_slots = [ { name: 'matrix', dt: core.datatypes.MATRIX, desc: 'The input matrix to be inverted.', def: 'Identity' } ];
-	this.output_slots = [ { name: 'inverse', dt: core.datatypes.MATRIX, desc: 'Emits the inverse of the input <b>matrix</b>.', def: 'Identity' } ];
-
-	this.reset = function()
-	{
-		self.matrix = mat4.create();
-		self.inverse = mat4.create();
-
-		mat4.identity(self.matrix);
-		mat4.identity(self.inverse);
-	};
 	
-	this.connection_changed = function(on, conn, slot)
-	{
-		if(!on && slot.type === E2.slot_type.input)
-		{
-			mat4.identity(this.matrix);
-			mat4.identity(this.inverse);
-		}
-	};	
+	this.input_slots = [
+		{ name: 'matrix', dt: core.datatypes.MATRIX, desc: 'The input matrix to be inverted.', def: 'Identity' }
+	];
+	
+	this.output_slots = [
+		{ name: 'inverse', dt: core.datatypes.MATRIX, desc: 'Emits the inverse of the input <b>matrix</b>.', def: 'Identity' }
+	];
+};
 
-	this.update_input = function(slot, data)
-	{
-		self.matrix = data;
-	};	
+E2.p.prototype.reset = function()
+{
+	this.matrix = mat4.create();
+	this.inverse = mat4.create();
 
-	this.update_state = function(delta_t)
-	{
-		mat4.inverse(self.matrix, self.inverse);
-	};	
+	mat4.identity(this.matrix);
+	mat4.identity(this.inverse);
+};
 
-	this.update_output = function(slot)
+E2.p.prototype.connection_changed = function(on, conn, slot)
+{
+	if(!on && slot.type === E2.slot_type.input)
 	{
-		return self.inverse;
-	};
+		mat4.identity(this.matrix);
+		mat4.identity(this.inverse);
+	}
+};	
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.matrix = data;
+};	
+
+E2.p.prototype.update_state = function(delta_t)
+{
+	mat4.inverse(this.matrix, this.inverse);
+};	
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.inverse;
 };
