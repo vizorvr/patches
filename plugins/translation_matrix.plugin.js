6dc49fb60d0d7a2a3787fffee78c50f84ee9d6b3
Lasse Nielsen <gaiaap@satcom3.(none)>
2012-11-30 02:20:19 +0100

    Refactored the translation matrix plugin to use prototype.

--- plugins/translation_matrix.plugin.js
+++ plugins/translation_matrix.plugin.js
@@ -1,28 +1,33 @@
-E2.plugins["translation_matrix"] = function(core, node) {
-	var self = this;
-	
+E2.p = E2.plugins["translation_matrix"] = function(core, node)
+{
 	this.desc = 'Create a matrix that represent a translation.';
-	this.input_slots = [ { name: 'vector', dt: core.datatypes.VECTOR, desc: 'Translation vector.', def: '0, 0, 0' } ];
-	this.output_slots = [ { name: 'matrix', dt: core.datatypes.MATRIX, desc: 'The resulting translation matrix.', def: 'Identity' } ];
 	
-	this.reset = function()
-	{
-		self.matrix = mat4.create();
+	this.input_slots = [
+		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'Translation vector.', def: '0, 0, 0' }
+	];
 	
-		mat4.identity(self.matrix);
-	};
-	
-	this.update_input = function(slot, data)
-	{
-		var m = self.matrix;
-		
-		m[12] = data[0];
-		m[13] = data[1];
-		m[14] = data[2];
-	};	
+	this.output_slots = [
+		{ name: 'matrix', dt: core.datatypes.MATRIX, desc: 'The resulting translation matrix.', def: 'Identity' }
+	];
+};
 
-	this.update_output = function(slot)
-	{
-		return self.matrix;
-	};
+E2.p.prototype.reset = function()
+{
+	this.matrix = mat4.create();
+
+	mat4.identity(this.matrix);
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	var m = this.matrix;
+	
+	m[12] = data[0];
+	m[13] = data[1];
+	m[14] = data[2];
+};	
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.matrix;
 };
