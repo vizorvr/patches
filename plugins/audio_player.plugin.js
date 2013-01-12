fe8bdbec77b2374d2fcfb1b3956256d170441f12
Lasse Nielsen <gaiaap@satcom3.(none)>
2013-01-12 23:43:00 +0100

    Update: Audio emitter. Additional input slots for control of playback muting (bool), and volume (float) added. Output slots supplying the current playback time and total audio duration in seconds added.

--- plugins/audio_player.plugin.js
+++ plugins/audio_player.plugin.js
@@ -4,14 +4,21 @@ E2.p = E2.plugins["audio_player"] = function(core, node)
 	
 	this.input_slots = [ 
 		{ name: 'audio', dt: core.datatypes.AUDIO, desc: 'The audio stream to play.' },
-		{ name: 'play', dt: core.datatypes.BOOL, desc: 'Send true to start playback and false to stop.' }
+		{ name: 'play', dt: core.datatypes.BOOL, desc: 'Send true to start playback and false to stop.', def: false },
+		{ name: 'mute', dt: core.datatypes.BOOL, desc: 'Send true to mute playback and false to reenable audio.', def: false },
+		{ name: 'volume', dt: core.datatypes.FLOAT, desc: 'Set playback volume.', lo: 0.0, hi: 0.0, def: 0.5 }
 	];
 	
-	this.output_slots = [];
-	
+	this.output_slots = [ 
+		{ name: 'current time', dt: core.datatypes.FLOAT, desc: 'The current audio playback position in seconds.' },
+		{ name: 'duration', dt: core.datatypes.FLOAT, desc: 'The audio duration in seconds.' }
+	];
+
 	this.audio = null;
 	this.playing = false;
 	this.should_play = false;
+	this.muted = false;
+	this.volume = 0.5;
 };
 
 E2.p.prototype.reset = function()
@@ -28,8 +35,12 @@ E2.p.prototype.update_input = function(slot, data)
 {
 	if(slot.index === 0)
 		this.audio = data;
-	else
+	else if(slot.index === 1)
 		this.should_play = data;
+	else if(slot.index === 2)
+		this.muted = data;
+	else if(slot.index === 3)
+		this.volume = data < 0.0 ? 0.0 : data > 1.0 ? 1.0 : data;
 };
 
 E2.p.prototype.update_state = function(delta_t)
@@ -46,4 +57,24 @@ E2.p.prototype.update_state = function(delta_t)
 		
 		this.playing = this.should_play;
 	}
+	
+	if(this.audio)
+	{
+		this.audio.muted = this.muted;
+		this.audio.volume = this.volume;
+	}
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	if(!this.audio)
+		return 0.0;
+		
+	if(this.playing)
+		this.updated = true;
+		
+	if(slot.index === 0)
+		return this.audio.currentTime;
+		
+	return this.audio.duration;
 };
