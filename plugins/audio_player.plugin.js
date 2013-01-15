110c2aa820a3f8e8a004625b72b06233760da977
Lasse Nielsen <gaiaap@satcom3.(none)>
2013-01-15 19:10:47 +0100

    Audio player: Removed the new output slots, since this plugin must function as an emitter, not a modulator. The lost functionality will be added as two separate audio modulators.

--- plugins/audio_player.plugin.js
+++ plugins/audio_player.plugin.js
@@ -6,19 +6,19 @@ E2.p = E2.plugins["audio_player"] = function(core, node)
 		{ name: 'audio', dt: core.datatypes.AUDIO, desc: 'The audio stream to play.' },
 		{ name: 'play', dt: core.datatypes.BOOL, desc: 'Send true to start playback and false to stop.', def: false },
 		{ name: 'mute', dt: core.datatypes.BOOL, desc: 'Send true to mute playback and false to reenable audio.', def: false },
-		{ name: 'volume', dt: core.datatypes.FLOAT, desc: 'Set playback volume.', lo: 0.0, hi: 0.0, def: 0.5 }
+		{ name: 'volume', dt: core.datatypes.FLOAT, desc: 'Set playback volume.', lo: 0.0, hi: 0.0, def: 0.5 },
+		{ name: 'time', dt: core.datatypes.FLOAT, desc: 'Set playback time.' }
 	];
 	
-	this.output_slots = [ 
-		{ name: 'current time', dt: core.datatypes.FLOAT, desc: 'The current audio playback position in seconds.' },
-		{ name: 'duration', dt: core.datatypes.FLOAT, desc: 'The audio duration in seconds.' }
-	];
-
+	this.output_slots = [
+	]
+	
 	this.audio = null;
 	this.playing = false;
 	this.should_play = false;
 	this.muted = false;
 	this.volume = 0.5;
+	this.time = null;
 };
 
 E2.p.prototype.reset = function()
@@ -41,40 +41,41 @@ E2.p.prototype.update_input = function(slot, data)
 		this.muted = data;
 	else if(slot.index === 3)
 		this.volume = data < 0.0 ? 0.0 : data > 1.0 ? 1.0 : data;
+	else if(slot.index === 4)
+		this.time = data;
 };
 
 E2.p.prototype.update_state = function(delta_t)
 {
+	var audio = this.audio;
+	
 	if(this.playing !== this.should_play)
 	{
-		if(this.audio)
+		if(audio)
 		{
 			if(this.should_play)
-				this.audio.play();
+				audio.play();
 			else
-				this.audio.pause();			
+				audio.pause();			
 		}
 		
 		this.playing = this.should_play;
 	}
 	
-	if(this.audio)
-	{
-		this.audio.muted = this.muted;
-		this.audio.volume = this.volume;
-	}
-};
+	if(!this.playing)
+		return;
 
-E2.p.prototype.update_output = function(slot)
-{
-	if(!this.audio)
-		return 0.0;
+	if(audio)
+	{
+		audio.muted = this.muted;
+		audio.volume = this.volume;
 		
-	if(this.playing)
-		this.updated = true;
-		
-	if(slot.index === 0)
-		return this.audio.currentTime;
-		
-	return this.audio.duration;
+		if(this.time)
+		{
+			audio.currentTime = this.time;
+			this.time = null;
+		}
+	}
+
+	this.updated = true;
 };
