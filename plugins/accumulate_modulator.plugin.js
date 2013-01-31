ee60421567d7f97ecef32455959fd30a8704f006
Lasse Nielsen <gaiaap@satcom3.(none)>
2013-01-31 17:40:21 +0100

    Added a reset input slot to the accumulator and clamped accumulator, to make it possible to reset them to any supplied value. The new slot has been added as the last input slot to maintain backwards compatibility.

--- plugins/accumulate_modulator.plugin.js
+++ plugins/accumulate_modulator.plugin.js
@@ -3,7 +3,8 @@ E2.p = E2.plugins["accumulate_modulator"] = function(core, node)
 	this.desc = 'Every input <b>value</b> is accumulated in an internal buffer (which resets to zero on playback stop).';
 	
 	this.input_slots = [
-		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'A small value to be accumulated in an internal buffer.', def: 0 }
+		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'A small value to be accumulated in an internal buffer.', def: 0 },
+		{ name: 'reset', dt: core.datatypes.FLOAT, desc: 'Send a value to this slot to reset the accumulator to that value.', def: 0 }
 	];
 	
 	this.output_slots = [
@@ -20,7 +21,10 @@ E2.p.prototype.reset = function()
 
 E2.p.prototype.update_input = function(slot, data)
 {
-	this.state.value += data;
+	if(slot.index === 0)
+		this.state.value += data;
+	else
+		this.state.value = data;
 };	
 
 E2.p.prototype.update_output = function(slot)
--- plugins/clamped_accumulate_modulator.plugin.js
+++ plugins/clamped_accumulate_modulator.plugin.js
@@ -6,7 +6,8 @@ E2.p = E2.plugins["clamped_accumulate_modulator"] = function(core, node)
 	[
 		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'A small value to be accumulated in an internal buffer.', def: 0 },
 		{ name: 'min', dt: core.datatypes.FLOAT, desc: 'Minimum internal buffer value.', def: 0 },
-		{ name: 'max', dt: core.datatypes.FLOAT, desc: 'Maximum internal buffer value.', def: 1 }
+		{ name: 'max', dt: core.datatypes.FLOAT, desc: 'Maximum internal buffer value.', def: 1 },
+		{ name: 'reset', dt: core.datatypes.FLOAT, desc: 'Send a value to this slot to reset the accumulator to that value.', def: 0 }
 	];
 	
 	this.output_slots = [
@@ -33,6 +34,8 @@ E2.p.prototype.update_input = function(slot, data)
 		this.lo = data;
 	if(slot.index === 2)
 		this.hi = data;
+	if(slot.index === 3)
+		this.state.value = data < this.lo ? this.lo : data > this.hi ? this.hi : data;
 };
 
 E2.p.prototype.update_output = function(slot)
