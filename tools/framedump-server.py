1f0ad95a997342d45f9d5812d41177c4b1145206
Lasse Nielsen <gaiaap@satcom3.(none)>
2013-02-01 15:06:14 +0100

    Udated the frame dump server with support for cross domain resource sharing to allow frame dumping to a local server, regardless of where Engi is being served from.

--- tools/framedump-server.py
+++ tools/framedump-server.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from flask import Flask
 from flask import request
+from flask import make_response
 from werkzeug import secure_filename
 from PIL import Image
 import numpy
@@ -9,20 +10,28 @@ app = Flask(__name__)
 
 index = 0
 
-@app.route('/upload', methods=['POST'])
+@app.route('/', methods=['POST'])
 def upload_file():
     global index
+    
     print request.method
+    
     if request.method == 'POST':
-        w = int(request.form['width'])
-        h = int(request.form['height'])
-        d = numpy.fromstring(request.form['img_data'][1:-1], dtype=numpy.uint8, sep=',');
-        fname = 'framedump/%06d.png' % index
-        print 'Writing %s (%d x %d)' % (fname, w, h)
-        img = Image.frombuffer("RGB", [w,h], d, 'raw')
-        img.save(fname)
-        index += 1
-        return '200'
+    	if request.headers['Origin']:
+    		print 'Allowing request to upload from \'' + request.headers['Origin'] + '\'.'
+		w = int(request.form['width'])
+		h = int(request.form['height'])
+		d = numpy.fromstring(request.form['img_data'][1:], dtype=numpy.uint8, sep=',');
+		fname = 'framedump/%06d.png' % index
+		print 'Writing %s (%d x %d), computed size = %d, actual size = %d' % (fname, w, h, w * h, len(d))
+		img = Image.frombuffer("RGB", [w,h], d, 'raw')
+		img.save(fname)
+		index += 1
+    		r = make_response('200')
+    		r.headers['Access-Control-Allow-Origin'] = '*'
+    		return r
+    	else:
+    		abort(404)
     
     abort(500)
 
