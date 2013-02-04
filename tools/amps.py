c4f4462bcddc890030edca29ce4f8ac1fc85ffe1
Lasse Nielsen <gaiaap@satcom3.(none)>
2013-02-04 23:17:01 +0100

    Improved tools/amps.py.

--- tools/amps.py
+++ tools/amps.py
@@ -3,8 +3,17 @@ import struct
 import wave
 import sys
 
+if len(sys.argv) < 2:
+	sys.exit('Usage: amps.py <inputfile.wav> (assumed to be 44.1Khz, 16bps signed, stereo and uncompressed)')
+	
 w = wave.open(sys.argv[1], 'rb')
-# We assume 44.1k @ 16-bit, can test with getframerate() and getsampwidth().
+
+if w.getframerate() != 44100:
+	sys.exit('Error: %s had a sample rate of %d, expected 44100.' % (sys.argv[1], w.getframerate()))
+	
+if w.getsampwidth() != 2:
+	sys.exit('Error: %s had a sample size of %d bytes, expected 2.' % (sys.argv[1], w.getsampwidth()))
+
 sum = 0
 value = 0;
 amps = []
@@ -20,13 +29,25 @@ def cnr(n):
 
     return s
     
-print '{ "data": [',
-delim = ''
+samples = []
+max_v = 0
 
 for i in xrange(0, w.getnframes()):
 	# Assume stereo, mix the channels.
 	data = struct.unpack('<hh', w.readframes(1))
-	sum += (data[0] + data[1]) / 65536.0
+	
+	f = abs(data[0])
+	
+	if f > max_v:
+		max_v = f
+	
+	f = abs(data[1])
+	
+	if f > max_v:
+		max_v = f
+	
+	sum += (data[0] + data[1]) / 32768.0
+
 	# 44100 / 30 = 1470
 	if (i != 0 and (i % 1470) == 0):
 		value = sum / 1470.0
@@ -37,11 +58,26 @@ for i in xrange(0, w.getnframes()):
 		if value < 0.00001:
 			value = 0.0
 
-		sys.stdout.write("%s%s" % (delim, cnr(value)))
-		sys.stdout.flush()
-		delim = ','
+		samples.append(value)
 		sum = 0
 
+max_v = max_v / 32768.0
+	
+if max_v < 0.00001:
+	max_v = 1.0
 
-print ']}'
+max_v = 1.0 / max_v
+
+delim = ''
+#print '{ "data": [',
+
+for i in xrange(0, len(samples)):
+	s = samples[i] * max_v
+	
+	sys.stdout.write("%s\n%s" % (delim, cnr(s)))
+	#sys.stdout.write("%s%s" % (delim, cnr(s)))
+	#sys.stdout.flush()
+	delim = ','
+
+#print ']}'
 
