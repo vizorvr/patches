d4b1c69c22cdb53d86ffc3a02b5d4ff8afefd5cc
Lasse Nielsen <gaiaap@satcom3.(none)>
2013-03-10 00:46:08 +0100

    Added vector dot and cross product plugins and their icons.

---
+++ plugins/vector_cross.plugin.js
@@ -0,0 +1,41 @@
+E2.p = E2.plugins["vector_cross"] = function(core, node)
+{
+	this.desc = 'Emits the cross products of the two supplied vectors.';
+	
+	this.input_slots = [ 
+		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'The first operand.', def: '0, 0, 0' }, 
+		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'The second operand.', def: '0, 0, 0' } 
+	];
+	
+	this.output_slots = [
+		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'Emits F cross S.', def: '0.0, 0.0, 0.0' }
+	];
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	if(slot.index === 0)
+		this.vector_a = data;
+	else if(slot.index === 1)
+		this.vector_b = data;
+};	
+
+E2.p.prototype.update_state = function()
+{
+	vec3.cross(this.vector_a, this.vector_b, this.result);
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.result;
+};	
+
+E2.p.prototype.state_changed = function(ui)
+{
+	if(!ui)
+	{
+		this.vector_a = [0, 0, 0];
+		this.vector_b = [0, 0, 0];
+		this.result = [0, 0, 0];
+	}
+};
---
+++ plugins/vector_dot.plugin.js
@@ -0,0 +1,41 @@
+E2.p = E2.plugins["vector_dot"] = function(core, node)
+{
+	this.desc = 'Emits the dot products of the two supplied vectors.';
+	
+	this.input_slots = [ 
+		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'The first operand.', def: '0, 0, 0' }, 
+		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'The second operand.', def: '0, 0, 0' } 
+	];
+	
+	this.output_slots = [
+		{ name: 'float', dt: core.datatypes.FLOAT, desc: 'Emits F dot S.', def: '0.0' }
+	];
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	if(slot.index === 0)
+		this.vector_a = data;
+	else if(slot.index === 1)
+		this.vector_b = data;
+};	
+
+E2.p.prototype.update_state = function()
+{
+	this.result = vec3.dot(this.vector_a, this.vector_b);
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.result;
+};	
+
+E2.p.prototype.state_changed = function(ui)
+{
+	if(!ui)
+	{
+		this.vector_a = [0, 0, 0];
+		this.vector_b = [0, 0, 0];
+		this.result = 0.0;
+	}
+};
