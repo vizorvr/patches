291f368b26f38d260101a1177f26889804f75cde
Lasse Nielsen <lasse@effekts.dk>
2014-02-20 16:12:19 +0100

    UI: Updated the expandable textfield implementation.

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,12 +1,12 @@
-function ExpandableTextfield(node, tf, def_width, cb)
+function ExpandableTextfield(node, tf, def_width)
 {
-	var handler = function(e)
+	var handler = function(node, tf, def_width) { return function(e)
 	{
 		var s = '' + tf.val();
 		
 		tf.css('width', '' + ((Math.max(def_width, s.length) * 7) + 2) + 'px');
 		node.geometry_updated();
-	};
+	}}(node, tf, def_width);
 	
 	tf.change(handler);
 	tf.keyup(handler);
