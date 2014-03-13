29d4dd572dc1064a4b52baabbe0469cd6b9c8fbb
Lasse Nielsen <lasse@effekts.dk>
2014-03-13 21:56:11 +0100

    Updated the equals plugin to be useable with any data type.

--- browser/plugins/equals_modulator.plugin.js
+++ browser/plugins/equals_modulator.plugin.js
@@ -3,20 +3,28 @@ E2.p = E2.plugins["equals_modulator"] = function(core, node)
 	this.desc = 'Emits true if <b>value</b> equals <b>reference</b> and false otherwise.';
 	
 	this.input_slots = [ 
-		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'Input value.', def: 0 },
-		{ name: 'reference', dt: core.datatypes.FLOAT, desc: 'Reference value to comprate <b>value</b> to.', def: 0 } 
+		{ name: 'value', dt: core.datatypes.ANY, desc: 'Input value.', def: null },
+		{ name: 'reference', dt: core.datatypes.ANY, desc: 'Reference value to comprate <b>value</b> to.', def: null } 
 	];
 	
 	this.output_slots = [
 		{ name: 'bool', dt: core.datatypes.BOOL, desc: 'True if <b>value</b> equals <b>reference</b> and false otherwise.', def: 'False' }
 	];
+
+	this.lsg = new LinkedSlotGroup(core, node, [this.input_slots[0], this.input_slots[1]], []);
 };
 
 E2.p.prototype.reset = function()
 {
-	this.value = 0.0;
-	this.ref = 0.0;
+	this.value = null;
+	this.ref = null;
 	this.state = false;
+};
+
+E2.p.prototype.connection_changed = function(on, conn, slot)
+{
+	if(this.lsg.connection_changed(on, conn, slot))
+		this.value = this.ref = this.lsg.core.get_default_value(this.lsg.dt);
 };
 
 E2.p.prototype.update_input = function(slot, data)
@@ -29,7 +37,7 @@ E2.p.prototype.update_input = function(slot, data)
 
 E2.p.prototype.update_state = function()
 {
-	this.state = this.value === this.ref;
+	this.state = (this.value === null || this.ref === null) ? false : this.value === this.ref;
 };
 
 E2.p.prototype.update_output = function(slot)
