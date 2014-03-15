c866f137fe26800d6f04122b67f5c1bd1b93b6aa
Lasse Nielsen <lasse@effekts.dk>
2014-03-15 16:09:53 +0100

    Added new plugins: Texture width and height modulators in Modulators/Texture/Height and Width.

---
+++ browser/plugins/texture_height_modulator.plugin.js
@@ -0,0 +1,29 @@
+E2.p = E2.plugins["texture_height_modulator"] = function(core, node)
+{
+	this.desc = 'Emits the supplied texture height.';
+	
+	this.input_slots = [
+		{ name: 'texture', dt: core.datatypes.TEXTURE, desc: 'The texture to emit the height of.' }
+	];
+	
+	this.output_slots = [ 
+		{ name: 'height', dt: core.datatypes.FLOAT, desc: 'The supplied texture height.' }
+	];
+	
+	this.height = 0;
+};
+
+E2.p.prototype.reset = function()
+{
+	this.height = 0;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.height = data ? parseInt(data.height, 10) : 0;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.height;
+};
---
+++ browser/plugins/texture_width_modulator.plugin.js
@@ -0,0 +1,29 @@
+E2.p = E2.plugins["texture_width_modulator"] = function(core, node)
+{
+	this.desc = 'Emits the supplied texture width.';
+	
+	this.input_slots = [
+		{ name: 'texture', dt: core.datatypes.TEXTURE, desc: 'The texture to emit the width of.' }
+	];
+	
+	this.output_slots = [ 
+		{ name: 'width', dt: core.datatypes.FLOAT, desc: 'The supplied texture width.' }
+	];
+	
+	this.width = 0;
+};
+
+E2.p.prototype.reset = function()
+{
+	this.width = 0;
+};
+
+E2.p.prototype.update_input = function(slot, data)
+{
+	this.width = data ? parseInt(data.width, 10) : 0;
+};
+
+E2.p.prototype.update_output = function(slot)
+{
+	return this.width;
+};
