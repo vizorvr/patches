99ea018d55f47a67a85b09f98c87f64c54b61e35
Lasse Nielsen <lasse@effekts.dk>
2014-04-06 17:11:32 +0200

    Updated the "array get as" modulator to sane slot defaults and to handle 'null' arrays, and fixed two bugs:
    * Wrong if / else in update_input could overwrite the index with an array.
    * Missing return statement lead to no output on slot #0 (array).

--- browser/plugins/array_get_as_modulator.plugin.js
+++ browser/plugins/array_get_as_modulator.plugin.js
@@ -3,7 +3,7 @@ E2.p = E2.plugins["array_get_as_modulator"] = function(core, node)
 	this.desc = 'Gets an item value from an array.';
 	
 	this.input_slots = [
-		{ name: 'array', dt: core.datatypes.ARRAY, desc: 'The array to obtain a value from.' },
+		{ name: 'array', dt: core.datatypes.ARRAY, desc: 'The array to obtain a value from.', def: null },
 		{ name: 'index', dt: core.datatypes.FLOAT, desc: 'The index of the item to get.', def: 0 }
 	];
 	
@@ -52,10 +52,13 @@ E2.p.prototype.create_ui = function()
 
 E2.p.prototype.update_input = function(slot, data)
 {
-	if(slot.index === 0 && this.array !== data)
+	if(slot.index === 0)
 	{
-		this.array = data;
-		this.update_view();
+		if(this.array !== data)
+		{
+			this.array = data;
+			this.update_view();
+		}
 	}
 	else
 		this.index = Math.floor(data);
@@ -63,6 +66,12 @@ E2.p.prototype.update_input = function(slot, data)
 
 E2.p.prototype.update_view = function()
 {
+	if(!this.array)
+	{
+		this.dv = null;
+		return;
+	}
+
 	var dv = this.dv = new DataView(this.array);
 
 	this.accessor = [dv.getInt8,
@@ -78,8 +87,11 @@ E2.p.prototype.update_view = function()
 E2.p.prototype.update_state = function()
 {
 	if(!this.dv)
+	{
+		this.value = 0;
 		return;
-
+	}
+	
 	var off = this.index * this.stride;
 	
 	if(off < 0 || off >= this.array.byteLength)
@@ -91,7 +103,7 @@ E2.p.prototype.update_state = function()
 E2.p.prototype.update_output = function(slot)
 {
 	if(slot.index === 0)
-		this.array;
+		return this.array;
 	else
 		return this.value;
 };
