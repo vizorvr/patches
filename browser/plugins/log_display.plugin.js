9182900bec2818e937f5dcc8f5a3fab80c936368
Lasse Nielsen <lasse@effekts.dk>
2014-04-07 16:48:19 +0200

    Fixed inputs of the log display, log, loop and lowpass filter plugins.

--- browser/plugins/log_display.plugin.js
+++ browser/plugins/log_display.plugin.js
@@ -3,7 +3,7 @@ E2.p = E2.plugins["log_display"] = function(core, node)
 	this.desc = 'Pipes the supplied text to the browser console log.';
 	
 	this.input_slots = [ 
-		{ name: 'text', dt: core.datatypes.TEXT, desc: 'Input text to be logged in the browsers console.' }
+		{ name: 'text', dt: core.datatypes.TEXT, desc: 'Input text to be logged in the browsers console.', def: null }
 	];
 	
 	this.output_slots = [];
@@ -11,5 +11,6 @@ E2.p = E2.plugins["log_display"] = function(core, node)
 
 E2.p.prototype.update_input = function(slot, data)
 {
-	console.log(data);
+	if(data)
+		console.log(data);
 };
--- browser/plugins/log_modulator.plugin.js
+++ browser/plugins/log_modulator.plugin.js
@@ -3,11 +3,11 @@ E2.p = E2.plugins["log_modulator"] = function(core, node)
 	this.desc = 'Log(x).';
 	
 	this.input_slots = [ 
-		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'Input value.', def: 0 }
+		{ name: 'value', dt: core.datatypes.FLOAT, desc: 'Input value.', def: 0.0 }
 	];
 	
 	this.output_slots = [
-		{ name: 'result', dt: core.datatypes.FLOAT, desc: 'log(<b>value</b>).', def: 0 }
+		{ name: 'result', dt: core.datatypes.FLOAT, desc: 'log(<b>value</b>).', def: 0.0 }
 	];
 };
 
