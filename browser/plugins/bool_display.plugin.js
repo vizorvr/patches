820e36e157b0582cf6704cc77b475f8d4d188bc6
Lasse Nielsen <lasse@effekts.dk>
2014-04-07 14:05:59 +0200

    Added fixed slot default to the bool display plugin and removed redundant connection_changed() implementation.

--- browser/plugins/bool_display.plugin.js
+++ browser/plugins/bool_display.plugin.js
@@ -3,7 +3,7 @@ E2.p = E2.plugins["bool_display"] = function(core, node)
 	this.desc = 'Displays the text \'True\' or \'False\' depending on the state of the supplied bool or \'-\', if no data is being received.';
 	
 	this.input_slots = [ 
-		{ name: 'bool', dt: core.datatypes.BOOL, desc: 'The input boolean to display.' }
+		{ name: 'bool', dt: core.datatypes.BOOL, desc: 'The input boolean to display.', def: null }
 	];
 	
 	this.output_slots = [];
@@ -23,12 +23,6 @@ E2.p.prototype.create_ui = function()
 	this.update_value(null);
 	
 	return this.label;
-};
-
-E2.p.prototype.connection_changed = function(on, conn, slot)
-{
-	if(!on)
-		this.update_value(null);
 };
 
 E2.p.prototype.update_input = function(slot, data)
