e69677e0d381ebb81873c2ea212b335b985bf6a9
Lasse Nielsen <lasse@effekts.dk>
2014-04-07 17:16:52 +0200

    Fixed inputs of the matrix clone and max plugins.

--- browser/plugins/matrix_clone_modulator.plugin.js
+++ browser/plugins/matrix_clone_modulator.plugin.js
@@ -3,7 +3,7 @@ E2.p = E2.plugins["matrix_clone_modulator"] = function(core, node)
 	this.desc = 'Makes a physical copy of the input matrix reference, to allow side-effect free transform chain branching.';
 	
 	this.input_slots = [ 
-		{ name: 'matrix', dt: core.datatypes.MATRIX, desc: 'Input matrix reference.' }
+		{ name: 'matrix', dt: core.datatypes.MATRIX, desc: 'Input matrix reference.', def: core.renderer.matrix_identity }
 	];
 	
 	this.output_slots = [
@@ -14,12 +14,6 @@ E2.p = E2.plugins["matrix_clone_modulator"] = function(core, node)
 E2.p.prototype.update_input = function(slot, data)
 {
 	this.matrix = mat4.create(data);
-};
-
-E2.p.prototype.connection_changed = function(on, conn, slot)
-{
-	if(!on && slot.type === E2.slot_type.input)
-		mat4.identity(this.matrix);
 };
 
 E2.p.prototype.update_output = function(slot)
