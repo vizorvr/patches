fefc3965ee17b859ad16088e44ec0df8a484d93e
Lasse Nielsen <lasse@effekts.dk>
2014-08-27 21:13:14 +0200

    Updated plugin presets and the script to (re)generate them.

--- tools/plugins-to-presets.js
+++ tools/plugins-to-presets.js
@@ -1,10 +1,15 @@
 var fs = require('fs');
-var plugins = require('/Users/anttijadertpolm/Documents/bootstrap/engi/browser/plugins/plugins.json');
+var plugins = require('../browser/plugins/plugins.json');
+var presets = [];
 
 for(var key in plugins)
 {
 	var pid = plugins[key];
 	var json = '{ "abs_t": 0, "active_graph": 0, "graph_uid": 1, "root": { "node_uid": 1, "uid": 0, "parent_uid": -1, "open": true, "nodes": [{ "plugin": "' + pid + '", "x": 50, "y": 50, "uid": 0}], "conns": [] }}';
+	var fname = pid.replace('_', '-');
 	
-	fs.writeFile('plugin-' + pid.replace('_', '-') + '.json', json);
+	fs.writeFile('../browser/presets/plugin-' + fname + '.json', json);
+	presets.push('\t\t"' + key + '": "' + fname + '"');
 }
+
+console.log(presets.join(',\n'));
