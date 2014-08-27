8f1a91327e933ebb91b58bc8fab0663e282156fd
Lasse Nielsen <lasse@effekts.dk>
2014-08-27 21:24:11 +0200

    Fixed presets script to use correct file prefix. Added binary build icon output files.

--- tools/plugins-to-presets.js
+++ tools/plugins-to-presets.js
@@ -6,9 +6,9 @@ for(var key in plugins)
 {
 	var pid = plugins[key];
 	var json = '{ "abs_t": 0, "active_graph": 0, "graph_uid": 1, "root": { "node_uid": 1, "uid": 0, "parent_uid": -1, "open": true, "nodes": [{ "plugin": "' + pid + '", "x": 50, "y": 50, "uid": 0}], "conns": [] }}';
-	var fname = pid.replace('_', '-');
+	var fname = 'plugin-' + pid.replace('_', '-');
 	
-	fs.writeFile('../browser/presets/plugin-' + fname + '.json', json);
+	fs.writeFile('../browser/presets/' + fname + '.json', json);
 	presets.push('\t\t"' + key + '": "' + fname + '"');
 }
 
