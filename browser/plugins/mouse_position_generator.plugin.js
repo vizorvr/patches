4d37916a8a8ddc98f04c187a5df980142e985b18
Lasse Nielsen <lasse@effekts.dk>
2014-09-17 11:16:09 +0200

    Changed the mouse position input plugin to emit coordinates in clip space since this makes creating UI elements much easier.
    Fixed misalignment in fullscreen mode.

--- browser/plugins/mouse_position_generator.plugin.js
+++ browser/plugins/mouse_position_generator.plugin.js
@@ -1,13 +1,15 @@
 E2.p = E2.plugins["mouse_position_generator"] = function(core, node)
 {
-	this.desc = 'Emits the current mouse position in unit space mapped to the canvas.';
+	this.desc = 'Emits the current mouse position in clip space mapped to the canvas.';
 	
 	this.input_slots = [];
 	
 	this.output_slots = [ 
-		{ name: 'x', dt: core.datatypes.FLOAT, desc: '0 at left edge of canvas and 1 at the right edge. Can be both smaller than 0 and larger than one if the mouse is outside the canvas bounds.', def: 0.0 },
-		{ name: 'y', dt: core.datatypes.FLOAT, desc: '0 at top edge of canvas and 1 at the bottom edge. Can be both smaller than 0 and larger than one if the mouse is outside the canvas bounds.', def: 0.0 } 
+		{ name: 'x', dt: core.datatypes.FLOAT, desc: '-1 at left edge of canvas and 1 at the right edge. Can be both smaller than -1 and larger than 1 if the mouse is outside the canvas bounds.', def: 0.0 },
+		{ name: 'y', dt: core.datatypes.FLOAT, desc: '-1 at top edge of canvas and 1 at the bottom edge. Can be both smaller than -1 and larger than 1 if the mouse is outside the canvas bounds.', def: 0.0 } 
 	];	
+	
+	this.renderer = core.renderer;
 };
 
 E2.p.prototype.update_output = function(slot)
@@ -21,9 +23,12 @@ E2.p.prototype.update_output = function(slot)
 E2.p.prototype.mouse_moved = function(self) { return function(e)
 {
 	var wgl_c = E2.dom.webgl_canvas[0]; 
+	var fs = self.renderer.fullscreen;
+	var w = fs ? wgl_c.clientWidth : wgl_c.width;
+	var h = fs ? wgl_c.clientHeight : wgl_c.height;
 	
-	self.x = (e.pageX - wgl_c.offsetLeft) / wgl_c.width;
-	self.y = (e.pageY - wgl_c.offsetTop) / wgl_c.height;
+	self.x = (((e.pageX - wgl_c.offsetLeft) / w) * 2.0) - 1.0;
+	self.y = ((1.0 - ((e.pageY - wgl_c.offsetTop) / h)) * 2.0) - 1.0;
 	self.updated = true;
 }};
 
