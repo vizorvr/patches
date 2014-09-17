f9b3e6ec1d3ba663070ed3b401291241169a55ce
Lasse Nielsen <lasse@effekts.dk>
2014-09-17 22:13:41 +0200

    Reverted the behavioral change to the preset selector that made it automatically reset when a preset was selected. Inserted semi-colons to make it more clear what was multi-line statements and what wasn't.

--- browser/scripts/collapsible-select-control.js
+++ browser/scripts/collapsible-select-control.js
@@ -1,13 +1,11 @@
-
 function CollapsibleSelectControl(handlebars) {
 	this._handlebars = handlebars || Handlebars;
-	this._template = this._handlebars.compile(
-		$("#collapsible-select-template").html());
-	this._cb = function() {}
+	this._template = this._handlebars.compile($("#collapsible-select-template").html());
+	this._cb = function() {};
 }
 
 CollapsibleSelectControl.prototype.data = function(d) {
-	this._data = d
+	this._data = d;
 	return this;
 }
 
@@ -21,67 +19,64 @@ CollapsibleSelectControl.prototype.focus = function() {
 }
 
 CollapsibleSelectControl.prototype._reset = function() {
-	$('.panel', this._el).show()
-	$('.panel li', this._el).show()
-	$('.collapse').collapse('hide')
+	$('.panel', this._el).show();
+	$('.panel li', this._el).show();
+	$('.collapse').collapse('hide');
 }
 
 CollapsibleSelectControl.prototype._search = function(text) {
 	if (!text) {
-		this._reset()
+		this._reset();
 		return;
 	}
 
-	$('.panel li', this._el).hide()
-	$('.panel', this._el).hide()
+	$('.panel li', this._el).hide();
+	$('.panel', this._el).hide();
 
-	var re = new RegExp(text, 'im')
+	var re = new RegExp(text, 'im');
 
 	var lis = $('.panel li', this._el)
 		.filter(function() {
 			return re.test(this.innerHTML)
-		})
+		});
 
-	lis.closest('.panel').show()
-	lis.closest('.panel-collapse').show()
-	lis.closest('.collapse').collapse('show')
+	lis.closest('.panel').show();
+	lis.closest('.panel-collapse').show();
+	lis.closest('.collapse').collapse('show');
 
-	lis.show()
+	lis.show();
 }
 
 CollapsibleSelectControl.prototype.render = function(el) {
-	var self = this
-	el = el || $('<div class="collapsible-select-control">').appendTo('body')
-	this._el = el
+	var self = this;
+	
+	el = el || $('<div class="collapsible-select-control">').appendTo('body');
+	this._el = el;
 
 	el.html(this._template({
 		controlId: "col-sel-"+Date.now(),
 		categories: this._data
-	}))
+	}));
 
 	el.on('hide.bs.collapse', function(e) {
 		$('.glyphicon', $(e.target).prev())
 			.removeClass('glyphicon-chevron-down')
-			.addClass('glyphicon-chevron-right')
-	})
+			.addClass('glyphicon-chevron-right');
+	});
 
 	el.on('show.bs.collapse', function(e) {
 		$('.glyphicon', $(e.target).prev())
 			.addClass('glyphicon-chevron-down')
-			.removeClass('glyphicon-chevron-right')
-	})
+			.removeClass('glyphicon-chevron-right');
+	});
 
 	$('input', el).on('keyup', function() {
-		self._search($('input', el).val())
-	})
+		self._search($('input', el).val());
+	});
 
 	$('li', el).dblclick(function(e) {
-		self._cb($(e.target).data('path'))
-		$('input', el).val('')
-		self._search('')
-	})
-
-	// this._reset();
+		self._cb($(e.target).data('path'));
+	});
 
 	return this;
 }
