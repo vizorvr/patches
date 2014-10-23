c9c579b87c9674103bd5569acb81b4aae6b3e460
Lasse Nielsen <lasse@effekts.dk>
2014-10-23 17:15:10 +0200

    Removed the texture instancing plugin and the TextureSampler class.

--- browser/scripts/renderer.js
+++ browser/scripts/renderer.js
@@ -13,38 +13,6 @@ Color.prototype.clone = function(src)
 	d[3] = s[3];
 };
 
-function TextureSampler(tex)
-{
-	var canvas = document.createElement('canvas');
-	var image = tex.image;
-	
-	canvas.width = image.width;
-	canvas.height = image.height;
-
-	var context = canvas.getContext('2d');
-
-	context.drawImage(image, 0, 0);
-
-	this.imgdata = context.getImageData(0, 0, image.width, image.height);
-	this.texture = tex;
-}
-
-TextureSampler.prototype.get_pixel = function(x, y)
-{
-	var img = this.texture.image;
-	
-	x = x < 0 ? 0 : x > 1.0 ? 1.0 : x;
-	y = y < 0 ? 0 : y > 1.0 ? 1.0 : y;
-
-	x *= img.width - 1;
-	y *= img.height - 1;
-	
-	var o = (Math.round(x) + (img.width * Math.round(y))) * 4;
-	var d = this.imgdata.data;
-	
-	return [d[o], d[o+1], d[o+2], d[o+3]];
-};
-
 function Texture(renderer, handle, filter)
 {
 	var gl = this.gl = renderer.context;
@@ -168,11 +136,6 @@ Texture.prototype.set_filtering = function(down, up)
 {
 	this.min_filter = down;
 	this.mag_filter = up;
-};
-
-Texture.prototype.get_sampler = function()
-{
-	return new TextureSampler(this);
 };
 
 function TextureCache(gl, core)
