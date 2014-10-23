e825f673e2afb80f6423a736a14f45f303e6a8e5
Lasse Nielsen <lasse@effekts.dk>
2014-10-23 19:14:36 +0200

    Moved ExpandableTextField from ui.js to util.js.

--- browser/scripts/util.js
+++ browser/scripts/util.js
@@ -128,6 +128,31 @@ function msg(txt)
 	d.scrollTop(d[0].scrollHeight);
 }
 
+function ExpandableTextfield(node, tf, def_width)
+{
+	var self = this;
+	
+	this.node = node;
+	this.tf = tf;
+	this.def_width = def_width;
+	
+	this.update = function()
+	{
+		var s = '' + self.tf.val();
+		
+		self.tf[0].style.width = ((Math.max(self.def_width, s.length) * 7) + 2) + 'px';
+		self.node.geometry_updated();
+	};
+	
+	var handler = function(self) { return function(e)
+	{
+		self.update();
+	}}(this);
+	
+	tf.change(handler);
+	tf.keyup(handler);
+}
+
 function load_script(url, onload, onerror)
 {
 	var script = document.createElement('script');
