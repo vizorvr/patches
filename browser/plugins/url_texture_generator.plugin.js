3f953d66deabe807d1e908c8be81c8394d1ecf16
crankshaft <mikko@crankshaft.fi>
2014-11-17 15:44:40 +0200

    Shows a thumbnail of the texture etc. Fixes #63

--- browser/plugins/url_texture_generator.plugin.js
+++ browser/plugins/url_texture_generator.plugin.js
@@ -1,10 +1,11 @@
 E2.p = E2.plugins["url_texture_generator"] = function(core, node)
 {
-	this.desc = 'Load a texture from an URL. JPEG and PNG supported. Hover over the Source button to see the url of the current file.';
+	this.desc = 'Load a texture from an URL. JPEG and PNG supported. Hover over the Change button to see the url of the current file.';
 	
-	this.input_slots = [
+	this.input_slots = [];
+/*	this.input_slots = [
 		{ name: 'url', dt: core.datatypes.TEXT, desc: 'Use this to load from a URL supplied as a string.', def: '' }
-	];
+	];*/
 	
 	this.output_slots = [
 		{ name: 'texture', dt: core.datatypes.TEXTURE, desc: 'The loaded texture.' }
@@ -15,6 +16,7 @@ E2.p = E2.plugins["url_texture_generator"] = function(core, node)
 	this.core = core;
 	this.texture = null;
 	this.dirty = false;
+	this.thumbnail = null;
 };
 
 E2.p.prototype.reset = function()
@@ -23,9 +25,23 @@ E2.p.prototype.reset = function()
 
 E2.p.prototype.create_ui = function()
 {
-	var inp = makeButton('Source', 'No texture selected.', 'url');
+	var container = make('div');
+	var inp = makeButton('Change', 'No texture selected.', 'url');
 	var self = this;
+
+	this.thumbnail = make('div');
 	
+	this.thumbnail.css({
+		'width': '71px',
+		'height': '71px',
+		'z-index': '3003',
+		'border': '2px solid #8e8e8e',
+		'border-radius': '5px',
+		'background-image': 'url(\'images/no_texture.png\')',
+		'background-size': 'cover',
+		'margin-bottom': '3px'
+  	});
+
 	inp.click(function()
 	{
 		FileSelectControl
@@ -39,7 +55,10 @@ E2.p.prototype.create_ui = function()
 			});
 	});
 
-	return inp;
+  	container.append(this.thumbnail);
+  	container.append(inp);
+
+  	return container;
 };
 
 E2.p.prototype.update_input = function(slot, data)
@@ -67,7 +86,10 @@ E2.p.prototype.state_changed = function(ui)
 	if(this.state.url !== '')
 	{
 		if(ui)
+		{
 			ui.attr('title', this.state.url);
+			this.thumbnail.css({ 'background-image': 'url(\'' + this.state.url + '\')' });
+		}
 		else
 			this.dirty = true;
 	}
