a63e7bac40e92dfd287964d44159e356fa3adbd2
Jaakko Manninen <kschzt@gmail.com>
2014-11-21 13:44:10 +0700

    use environment variable GRIDFS url; have Dockerfile install gm

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,12 @@ FROM node
 
 ENV HOME /root
 ENV NODE_ENV production
+ENV MONGODB mongodb://mongo:27017/engi
+ENV GRIDFS mongodb://mongo:27017/engi-assets
 EXPOSE 8000
+
+RUN apt-get -q update
+RUN apt-get install -y graphicsmagick
 
 CMD [ "node", "app.js" ]
 
--- config/secrets.js
+++ config/secrets.js
@@ -18,5 +18,6 @@
 
 module.exports = {
   db: process.env.MONGODB|| 'mongodb://localhost:27017/engi',
+  gridFs: process.env.GRIDFS|| 'mongodb://localhost:27017/engi-assets',
   sessionSecret: process.env.SESSION_SECRET || ', my Session Secr!t is Wv,xclzc jhut6 wtf psod lol'
 };
