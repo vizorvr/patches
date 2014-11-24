60251309ef0617fdae7e872402abab841ed19323
Lars Kluge <l@larskluge.com>
2014-11-24 20:15:59 +0700

    Minify all js required for player into one file

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,64 @@
-var gulp = require('gulp');
-var fs = require('fs');
-var uglify = require('gulp-uglify');
-var concat = require('gulp-concat');
+var
+  gulp = require('gulp'),
+  fs = require('fs'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  del = require('del'),
+  paths = {
+    js: {
+      player: [
+        './browser/vendor/gl-matrix.js',
+        './browser/scripts/util.js',
+        './browser/scripts/renderer.js',
+        './browser/scripts/plugin-group.js',
+        './browser/scripts/plugin-manager.js',
+        './browser/scripts/connection.js',
+        './browser/scripts/graph.js',
+        './browser/scripts/node.js',
+        './browser/scripts/registers.js',
+        './browser/scripts/core.js',
+        './browser/scripts/player.js',
+        './browser/scripts/player-run.js'
+      ]
+    }
+  }
 
-gulp.task('plugins', function()
+
+
+
+gulp.task('clean:js:plugins', function(cb)
 {
-	try {
-		fs.unlinkSync('./browser/plugins/all.plugins.js');
-	} catch(e) {}
+  del('./browser/plugins/all.plugins.js', cb);
+});
 
+gulp.task('clean:js:player', function(cb)
+{
+  del('./browser/scripts/player.min.js', cb);
+});
+
+gulp.task('clean:js', ['clean:js:plugins', 'clean:js:player']);
+
+gulp.task('clean', ['clean:js']);
+
+
+gulp.task('js:plugins', ['clean:js:plugins'], function()
+{
 	gulp.src('browser/plugins/*.js')
 		.pipe(uglify())
 		.pipe(concat('all.plugins.js'))
 		.pipe(gulp.dest('./browser/plugins/'))
 });
+
+gulp.task('js:player', ['clean:js:player'], function()
+{
+  gulp.src(paths.js.player)
+		.pipe(uglify())
+		.pipe(concat('player.min.js'))
+		.pipe(gulp.dest('./browser/scripts/'))
+});
+
+gulp.task('js', ['js:plugins', 'js:player']);
+
+
+gulp.task('default', ['js']);
+
