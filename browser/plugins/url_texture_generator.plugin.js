1abc2d223c7119fd12c2d52a1240e89aa725d6a1
Jaakko Manninen <kschzt@gmail.com>
2015-03-04 17:07:18 +0200

    re-enable UrlTexture plugin url input, update code style

--- browser/plugins/url_texture_generator.plugin.js
+++ browser/plugins/url_texture_generator.plugin.js
@@ -1,35 +1,33 @@
-E2.p = E2.plugins["url_texture_generator"] = function(core, node)
-{
-	this.desc = 'Load a texture from an URL. JPEG and PNG supported. Hover over the Change button to see the url of the current file.';
+(function() {
+
+var UrlTexture = E2.plugins['url_texture_generator'] = function(core, node) {
+	this.desc = 'Load a texture from a URL. JPEG and PNG supported. Hover over the Browse button to select an existing image from the library.'
 	
-	this.input_slots = [];
-/*	this.input_slots = [
-		{ name: 'url', dt: core.datatypes.TEXT, desc: 'Use this to load from a URL supplied as a string.', def: '' }
-	];*/
+	this.input_slots = []
+	this.input_slots = [
+		{ name: 'url', dt: core.datatypes.TEXT,
+			desc: 'URL to fetch image from. The image width and height must be in powers of two, for example 256x512.',
+			def: '' }
+	]
 	
 	this.output_slots = [
 		{ name: 'texture', dt: core.datatypes.TEXTURE, desc: 'The loaded texture.' }
-	];
+	]
 	
-	this.state = { url: '' };
-	this.gl = core.renderer.context;
-	this.core = core;
-	this.texture = null;
-	this.dirty = false;
-	this.thumbnail = null;
-};
+	this.state = { url: '' }
+	this.gl = core.renderer.context
+	this.core = core
+	this.texture = null
+	this.dirty = false
+	this.thumbnail = null
+}
 
-E2.p.prototype.reset = function()
-{
-};
+UrlTexture.prototype.create_ui = function() {
+	var container = make('div')
+	var inp = makeButton('Browse', 'No texture selected.', 'url')
+	var that = this
 
-E2.p.prototype.create_ui = function()
-{
-	var container = make('div');
-	var inp = makeButton('Change', 'No texture selected.', 'url');
-	var self = this;
-
-	this.thumbnail = make('div');
+	this.thumbnail = make('div')
 	
 	this.thumbnail.css({
 		'width': '71px',
@@ -40,57 +38,48 @@ E2.p.prototype.create_ui = function()
 		'background-image': 'url(\'images/no_texture.png\')',
 		'background-size': 'cover',
 		'margin-bottom': '3px'
-  	});
+	})
 
-	inp.click(function()
-	{
+	inp.click(function() {
 		FileSelectControl
-			.createTextureSelector(self.state.url)
-			.onChange(function(v)
-			{
-				self.state.url = v;
-				self.state_changed(null);
-				self.state_changed(inp);
-				self.updated = true;
-			});
-	});
+			.createTextureSelector(that.state.url)
+			.onChange(function(v) {
+				that.state.url = v
+				that.state_changed()
+				that.updated = true
+			})
+	})
 
-  	container.append(this.thumbnail);
-  	container.append(inp);
+	container.append(this.thumbnail)
+	container.append(inp)
 
-  	return container;
-};
+	return container
+}
 
-E2.p.prototype.update_input = function(slot, data)
-{
-	this.state.url = data;
-	this.state_changed(null);
-};
+UrlTexture.prototype.update_input = function(slot, data) {
+	this.state.url = data
+	this.state_changed()
+}
 
-E2.p.prototype.update_state = function()
-{
-	if(!this.dirty)
-		return;
+UrlTexture.prototype.update_state = function() {
+	if (!this.dirty)
+		return
 
-	this.texture = this.core.renderer.texture_cache.get(this.state.url);
-	this.dirty = false;
-};
+	this.texture = this.core.renderer.texture_cache.get(this.state.url)
+	this.dirty = false
+}
 
-E2.p.prototype.update_output = function(slot)
-{
-	return this.texture;
-};
+UrlTexture.prototype.update_output = function(slot) {
+	return this.texture
+}
 
-E2.p.prototype.state_changed = function(ui)
-{
-	if(this.state.url !== '')
-	{
-		if(ui)
-		{
-			ui.attr('title', this.state.url);
-			this.thumbnail.css({ 'background-image': 'url(\'' + this.state.url + '\')' });
-		}
-		else
-			this.dirty = true;
+UrlTexture.prototype.state_changed = function() {
+	if (this.state.url !== '') {
+		if (this.thumbnail) {
+			this.thumbnail.css({ 'background-image': 'url(\'' + this.state.url + '\')' })
+		} else
+			this.dirty = true
 	}
-};
+}
+
+})()
