da66e66239dc323b242ae7a41ccc4a481b2f59cc
Jaakko Manninen <kschzt@gmail.com>
2015-03-11 19:06:27 +0200

    LoadGraph: dont allow loading empty objects

--- browser/plugins/load_graph.plugin.js
+++ browser/plugins/load_graph.plugin.js
@@ -6,9 +6,6 @@ var LoadGraph = E2.plugins['load_graph'] = function(core, node) {
 	this.desc = 'Instructs the player to play the graph (replacing the current graph) when enabled'
 
 	this.input_slots = [{
-			name: 'trigger', dt: core.datatypes.BOOL,
-			desc: 'Load the graph when true', def: null
-		}, {
 			name: 'back key', dt: core.datatypes.FLOAT,
 			desc: 'If set, pressing this key will load the previous graph (space=32)',
 			def: null
@@ -21,16 +18,14 @@ var LoadGraph = E2.plugins['load_graph'] = function(core, node) {
 	this.output_slots = []
 }
 
-LoadGraph.prototype.play = function() {
-	if (!prevGraph) {
-		prevGraph = E2.app.player.core.serialise()
-	}
-}
-
 LoadGraph.prototype.update_input = function(slot, data) {
 	var that = this
 
 	function goToGraph() {
+		if (!prevGraph) {
+			prevGraph = E2.app.player.core.serialise()
+		}
+
 		function onKeyPress(e) {
 			if (e.keyCode !== that._keyCode)
 				return;
@@ -53,22 +48,13 @@ LoadGraph.prototype.update_input = function(slot, data) {
 
 	switch(slot.index) {
 		case 0:
-			if (!this._graph) {
-				// go to graph when ready
-				this._triggered = true
-				return;
-			}
-
-			goToGraph()
-
-			break;
-		case 1:
 			this._keyCode = Math.floor(data)
 			break;
-		case 2:
+		case 1:
+			if (!data || !Object.keys(data).length)
+				return;
 			this._graph = data
-			if (this._triggered)
-				goToGraph()
+			goToGraph()
 			break;
 	}
 }	
