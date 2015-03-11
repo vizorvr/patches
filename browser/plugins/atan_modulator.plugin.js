72b49b466c2474fb4abdaa307719438d5e49bb5b
kaarlo <kkananen@googlemail.com>
2015-03-11 09:07:26 +0000

    separate atan & atan2, fix atan2 maths

--- browser/plugins/atan_modulator.plugin.js
+++ browser/plugins/atan_modulator.plugin.js
@@ -1,34 +1,27 @@
 E2.p = E2.plugins["atan_modulator"] = function(core, node)
 {
-	this.desc = 'Atan(x, y).';
+	this.desc = 'Atan(x).';
 	
 	this.input_slots = [ 
-		{ name: 'x value', dt: core.datatypes.FLOAT, desc: 'X value.', def: 0.0 },
-		{ name: 'y value', dt: core.datatypes.FLOAT, desc: 'Y value.', def: 0.0 }
+		{ name: 'x value', dt: core.datatypes.FLOAT, desc: 'X value.', def: 0.0 }
 	];
 	
 	this.output_slots = [
-		{ name: 'result', dt: core.datatypes.FLOAT, desc: 'atan(<b>x</b>,<b>y</b>).', def: 0.0 }
+		{ name: 'result', dt: core.datatypes.FLOAT, desc: 'atan(<b>x</b>).', def: 0.0 }
 	];
 };
 
 E2.p.prototype.reset = function()
 {
-	this.x_value = 0.0;
-	this.y_value = 0.0;
+	this.value = 0.0;
 };
 
 E2.p.prototype.update_input = function(slot, data)
 {
-  if (slot.index === 0)
-	  this.x_value = data;
-  else
-    this.y_value = data;
+  this.value = Math.atan(data);
 };	
 
 E2.p.prototype.update_output = function(slot)
 {
-  this.value = Math.atan2(this.y_value, this.x_value);
-
 	return this.value;
 };
