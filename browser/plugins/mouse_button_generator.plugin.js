c07b839841df8e28c397f92daa8079654474a1fd
Jaakko Manninen <kschzt@gmail.com>
2015-03-31 17:08:07 -0700

    mouse_button_generator style

--- browser/plugins/mouse_button_generator.plugin.js
+++ browser/plugins/mouse_button_generator.plugin.js
@@ -1,41 +1,40 @@
-E2.p = E2.plugins["mouse_button_generator"] = function(core, node)
-{
-	this.desc = 'Emits the current mouse button state.';
+(function() {
+
+var MouseButtons = E2.plugins["mouse_button_generator"] = function(core, node) {
+	this.desc = 'Emits the current mouse button state.'
 	
-	this.input_slots = [];
+	this.input_slots = []
 	
 	this.output_slots = [ 
 		{ name: 'Left', dt: core.datatypes.BOOL, desc: 'True if the left mouse button is pressed and false otherwise.', def: false },
 		{ name: 'Middle', dt: core.datatypes.BOOL, desc: 'True if the middle mouse button is pressed and false otherwise.', def: false },
 		{ name: 'Right', dt: core.datatypes.BOOL, desc: 'True if the right mouse button is pressed and false otherwise.', def: false }
-	];	
-};
+	]
+}
 
-E2.p.prototype.update_output = function(slot)
-{
-	return this.buttons[slot.index];
-};
+MouseButtons.prototype.update_output = function(slot) {
+	return this.buttons[slot.index]
+}
 
-E2.p.prototype.mouse_down = function(self) { return function(e)
-{
-	e.preventDefault();
-	self.buttons[e.which - 1] = true;
-	self.updated = true;
-}};
+MouseButtons.prototype.mouse_down = function(e) {
+	e.preventDefault()
+	this.buttons[e.which - 1] = true
+	this.updated = true
+}
 
-E2.p.prototype.mouse_up = function(self) { return function(e)
-{
-	self.buttons[e.which - 1] = false;
-	self.updated = true;
-}};
+MouseButtons.prototype.mouse_up = function(e) {
+	this.buttons[e.which - 1] = false
+	this.updated = true
+}
 
-E2.p.prototype.state_changed = function(ui)
-{
-	if(!ui)
-	{
-		this.buttons = [];
-		this.buttons[0] = this.buttons[1] = this.buttons[2] = false;
-		E2.dom.webgl_canvas.mousedown(this.mouse_down(this));
-		E2.dom.webgl_canvas.mouseup(this.mouse_up(this));
+MouseButtons.prototype.state_changed = function(ui) {
+	if (!ui) {
+		this.buttons = []
+		this.buttons[0] = this.buttons[1] = this.buttons[2] = false
+
+		E2.dom.webgl_canvas.on('mousedown', this.mouse_down.bind(this))
+		E2.dom.webgl_canvas.on('mouseup', this.mouse_up.bind(this))
 	}
-};
+}
+
+})
