35fd935e57fb2ab4db5396e2c0df5ec6a277abe9
Jaakko Manninen <kschzt@gmail.com>
2015-04-21 22:02:10 -0700

    add AbstractPlugin with undoableSetState method, extend in Slider, Const and Texture

--- browser/plugins/const_float_generator.plugin.js
+++ browser/plugins/const_float_generator.plugin.js
@@ -1,4 +1,7 @@
-E2.p = E2.plugins["const_float_generator"] = function(core, node) {
+(function(){
+var Const = E2.plugins.const_float_generator = function(core, node) {
+	AbstractPlugin.apply(this, arguments)
+
 	this.desc = 'Emits a float constant specified in an input field. If an invalid string in entered, the field is reset to the previous value.';
 	
 	this.input_slots = [];
@@ -11,10 +14,11 @@ E2.p = E2.plugins["const_float_generator"] = function(core, node) {
 	this.core = core;
 	this.node = node;
 };
+Const.prototype = Object.create(AbstractPlugin.prototype)
 
-E2.p.prototype.reset = function() {}
+Const.prototype.reset = function() {}
 
-E2.p.prototype.create_ui = function() {
+Const.prototype.create_ui = function() {
 	var that = this
 	var inp = this.$input = 
 		$('<input type="number" step="0.1" value="1.0" style="width: 50px;" />')
@@ -32,14 +36,7 @@ E2.p.prototype.create_ui = function() {
 			return;
 		}
 
-		E2.app.undoManager.execute(
-			new E2.commands.graph.ChangePluginState(
-				that.node.parent_graph,
-				that.node,
-				'val',
-				oldValue,
-				newValue
-		))
+		that.undoableSetState('val', newValue, oldValue)
 	})
 
 	this.etf = new ExpandableTextfield(this.node, inp, 7)
@@ -47,12 +44,14 @@ E2.p.prototype.create_ui = function() {
 	return inp;
 }
 
-E2.p.prototype.update_output = function(slot) {
+Const.prototype.update_output = function(slot) {
 	return this.state.val
 }
 
-E2.p.prototype.state_changed = function(ui) {
+Const.prototype.state_changed = function(ui) {
 	if (ui) {
 		ui.val('' + this.state.val)
 	}
 }
+
+})()
