f809a1ea94dd4fdd5d8410830062d1b1a2cdac18
Jaakko Manninen <kschzt@gmail.com>
2015-04-28 11:45:34 -0700

    disable wsChannel for now

--- browser/scripts/editorChannel.js
+++ browser/scripts/editorChannel.js
@@ -4,19 +4,22 @@ function EditorChannel(app) {
 
 	var that = this
 	this.api = app.graphApi
-
+/*
 	wsChannel
 		.join('__editor__')
 		.on('*', function(m) {
 			console.log('EditorChannel IN: ', m.type)
 			this.emit.apply(this, [m.type].concat(m.objects))
 		}.bind(this))
+*/
 }
 
 EditorChannel.prototype = Object.create(EventEmitter.prototype)
 
 EditorChannel.prototype.broadcast = function(m) {
 	console.log('broadcast', m.type)
-	wsChannel.send('__editor__', m)
+	// wsChannel.send('__editor__', m)
 }
 
+if (typeof(module) !== 'undefined')
+	module.exports = EditorChannel
