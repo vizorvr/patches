3aa0d4092febfce78052ab57ebd3fe580544cea1
Jaakko Manninen <kschzt@gmail.com>
2015-05-06 22:10:00 -0700

    annotation plugin style fix

--- browser/plugins/annotation.plugin.js
+++ browser/plugins/annotation.plugin.js
@@ -1,57 +1,58 @@
 (function(){
-Annotation = E2.plugins.annotation = function() {
-	Plugin.apply(this, arguments)
-	this.desc = 'Add textual hints to the graph.';
-	
-	this.input_slots = [];
-	
-	this.output_slots = [];
-	
-	this.state = { text: '', width: 0, height: 0 };
-};
-Annotation.prototype = Object.create(Plugin.prototype)
-
-Annotation.prototype.create_ui = function() {
-	var that = this
-	var inp = $('<textarea placeholder="Type text here" />');
-	
-	inp.css({
-		'font-size': '8pt',
-		'border': '1px solid #999',
-		'margin': '0px',
-		'margin-top': '2px',
-		'padding': '2px',
-		'resize': true
-	});
-	
-	inp.on('change', function() {
-		that.undoableSetState('text', inp.val(), that.state.text)
-	})
-	
-	// Chrome doesn't handle resize properly for anything but the window object,
-	// so we store the potentially altered size of the textarea on mouseup.
-	inp.mouseup(function() {
-		var ta = $(this);
-
-		that.state.width = ta.width();
-		that.state.height = ta.height();
-	})
-	
-	return inp;
-}
-
-Annotation.prototype.state_changed = function(ui) {
-	var s = this.state;
-	
-	if(ui && s.text !== '')
-	{
-		ui.val(s.text);
+	var Annotation = E2.plugins.annotation = function() {
+		Plugin.apply(this, arguments)
+		this.desc = 'Add textual hints to the graph.';
 		
-		if(s.width > 0)
-			ui.css('width', s.width);
+		this.input_slots = [];
 		
-		if(s.height > 0)
-			ui.css('height', s.height);
+		this.output_slots = [];
+		
+		this.state = { text: '', width: 0, height: 0 };
+	};
+	Annotation.prototype = Object.create(Plugin.prototype)
+
+	Annotation.prototype.create_ui = function() {
+		var that = this
+		var inp = $('<textarea placeholder="Type text here" />');
+		
+		inp.css({
+			'font-size': '8pt',
+			'border': '1px solid #999',
+			'margin': '0px',
+			'margin-top': '2px',
+			'padding': '2px',
+			'resize': true
+		});
+		
+		inp.on('change', function() {
+			that.undoableSetState('text', inp.val(), that.state.text)
+		})
+		
+		// Chrome doesn't handle resize properly for anything but the window object,
+		// so we store the potentially altered size of the textarea on mouseup.
+		inp.mouseup(function() {
+			var ta = $(this);
+
+			that.state.width = ta.width();
+			that.state.height = ta.height();
+		})
+		
+		return inp;
 	}
-};
-})()
+
+	Annotation.prototype.state_changed = function(ui) {
+		var s = this.state;
+		
+		if(ui && s.text !== '')
+		{
+			ui.val(s.text);
+			
+			if(s.width > 0)
+				ui.css('width', s.width);
+			
+			if(s.height > 0)
+				ui.css('height', s.height);
+		}
+	}
+})();
+
