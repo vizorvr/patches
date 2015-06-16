1ea5d8c906ce1d5ea9cfac44019fa380b1f9752f
Jaakko Manninen <kschzt@gmail.com>
2015-06-16 14:57:55 +0300

    fix password reset process

--- config/secrets.js
+++ config/secrets.js
@@ -1,5 +1,6 @@
 module.exports = {
 	db: process.env.MONGODB || 'mongodb://localhost:27017/vizor',
 	gridFs: process.env.GRIDFS || 'mongodb://localhost:27017/vizor-assets',
-	sessionSecret: process.env.SESSION_SECRET || 'vizor'
+	sessionSecret: process.env.SESSION_SECRET || 'vizor',
+	mandrill: process.env.MANDRILL || 'mandrillSecretNotThere',
 };
