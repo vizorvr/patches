774bc6cfe0af97f76e3749928b2de010c3462ec3
Jaakko Manninen <kschzt@gmail.com>
2015-08-17 19:19:47 -0700

    don`t object-pick if noodles visible

--- browser/plugins/three_webgl_renderer.plugin.js
+++ browser/plugins/three_webgl_renderer.plugin.js
@@ -70,7 +70,7 @@
 	}
 
 	ThreeWebGLRendererPlugin.prototype.pick_object = function(e) {
-		if (E2.app.noodlesVisible === true)
+		if (E2.app.noodlesVisible)
 			return;
 
 		var mouseVector = new THREE.Vector3()
