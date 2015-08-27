8eb4453ec0ec48c046f7d72bcdbbba96e42cb32c
Jaakko Manninen <kschzt@gmail.com>
2015-08-27 15:10:27 -0700

    fix shading slot text, add shading slot to lambert material, fixes #299

--- browser/plugins/three_material.plugin.js
+++ browser/plugins/three_material.plugin.js
@@ -11,10 +11,9 @@
 			{	name: 'fog', dt: core.datatypes.BOOL, def: true,
 				desc: 'Define whether the material color is affected by global fog settings.'
 			},
-			{	name: 'shading', dt: core.datatypes.FLOAT,
-				desc: 'Defines which of the face sides will be rendered - front, back or both. ' +
-					'0 = Front, 1 = Back, 2 = Double Sided',
-				def: 0
+			{	name: 'shading', dt: core.datatypes.FLOAT, def: 2,
+				desc: 'Define shading type. 0 = No Shading, '+
+					'1 = Flat Shading, 2 = Smooth Shading.'
 			},
 		].concat(this.input_slots)
 
@@ -30,7 +29,6 @@
 		AbstractThreeMaterialPlugin.prototype.reset.call(this)
 		this.material = new THREE.MeshBasicMaterial()
 	}
-
 
 })()
 
--- browser/plugins/three_material_lambert.plugin.js
+++ browser/plugins/three_material_lambert.plugin.js
@@ -5,10 +5,17 @@
 		this.desc = 'THREE.js Lambert Material'
 		
 		this.input_slots = [
-			{ name: 'texture', dt: core.datatypes.TEXTURE },
-			{ name: 'color', dt: core.datatypes.COLOR },
-			{ name: 'lightMap', dt: core.datatypes.TEXTURE },
-			{ name: 'wireframe', dt: core.datatypes.BOOL },
+			{	name: 'texture', dt: core.datatypes.TEXTURE },
+			{	name: 'color', dt: core.datatypes.COLOR },
+			{	name: 'lightMap', dt: core.datatypes.TEXTURE },
+			{	name: 'wireframe', dt: core.datatypes.BOOL },
+			{	name: 'fog', dt: core.datatypes.BOOL, def: true,
+				desc: 'Define whether the material color is affected by global fog settings.'
+			},
+			{	name: 'shading', dt: core.datatypes.FLOAT, def: 2,
+				desc: 'Define shading type. 0 = No Shading, '+
+					'1 = Flat Shading, 2 = Smooth Shading.'
+			},
 		].concat(this.input_slots)
 	}
 
@@ -18,7 +25,6 @@
 		AbstractThreeMaterialPlugin.prototype.reset.call(this)
 		this.material = new THREE.MeshLambertMaterial()
 	}
-
 
 })()
 
