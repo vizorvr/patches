70b8c506ea85061bcd62381170810c00b21f9ee8
Kaarlo Kananen <kkananen@gmail.com>
2015-10-02 15:50:39 -0700

    split runtime event emitter into two - a single and a continuous emitter and add tests

--- browser/test/integration/runtimeEvents.js
+++ browser/test/integration/runtimeEvents.js
@@ -62,7 +62,7 @@ global.ConnectionUI.prototype.resolve_slot_divs = function() {
 }
 global.navigator = { userAgent: 'test' }
 
-describe('Array function', function() {
+describe('Runtime events', function() {
 	
 	global.window = global
 
@@ -119,7 +119,7 @@ describe('Array function', function() {
 
 	})
 
-	it('triggers for one frame only when system emits', function() {
+	it('trigger for one frame only when system emits', function() {
 		var graph = E2.core.active_graph
 
 		E2.app.instantiatePlugin('const_text_generator')
@@ -165,7 +165,7 @@ describe('Array function', function() {
 		assert.equal(0, floatNode.plugin.value)
 	})
 
-	it('triggers for one frame only when writer emits', function() {
+	it('trigger for one frame only when writer emits', function() {
 		var graph = E2.core.active_graph
 
 		E2.app.instantiatePlugin('runtime_event_write')
@@ -224,6 +224,87 @@ describe('Array function', function() {
 		assert.equal(0, floatNode.plugin.value)
 	})
 
+	it('trigger for multiple frames when continuous writer emits', function() {
+		var graph = E2.core.active_graph
+
+		E2.app.instantiatePlugin('runtime_event_write_continuous')
+
+		E2.app.instantiatePlugin('const_text_generator')
+		E2.app.instantiatePlugin('runtime_event_read')
+		E2.app.instantiatePlugin('float_display')
+
+		var emitNode = graph.nodes[0]
+		var textNode = graph.nodes[1]
+		var readNode = graph.nodes[2]
+		var floatNode = graph.nodes[3]
+
+		// connect read output to float
+		var c = Connection.hydrate(graph, {
+			src_nuid: readNode.uid,
+			dst_nuid: floatNode.uid,
+			src_slot: 0,
+			dst_slot: 0,
+		})
+
+		readNode.plugin.connection_changed(true,
+		c, readNode.plugin.output_slots[0])
+
+		// connect text.output to emitNode.eventName
+		Connection.hydrate(graph, {
+			src_nuid: textNode.uid,
+			dst_nuid: emitNode.uid,
+			src_slot: 0,
+			dst_slot: 1,
+		})
+
+		// connect text.output to readNode.eventName
+		Connection.hydrate(graph, {
+			src_nuid: textNode.uid,
+			dst_nuid: readNode.uid,
+			src_slot: 0,
+			dst_slot: 0,
+		})
+
+		// set text value
+		textNode.plugin.state.text = 'testEventName'
+
+		graph.update()
+
+		var epl = emitNode.plugin
+		epl.update_input(epl.input_slots[0], true)
+		epl.update_input(epl.input_slots[2], 79.21)
+
+		graph.update()
+
+		// two frames with a value
+		assert.equal(79.21, floatNode.plugin.value)
+
+		graph.update()
+
+		assert.equal(79.21, floatNode.plugin.value)
+
+		// switch emitter off for two frames
+		epl.update_input(epl.input_slots[0], false)
+
+		graph.update()
+
+		assert.equal(0, floatNode.plugin.value)
+
+		graph.update()
+
+		assert.equal(0, floatNode.plugin.value)
+
+		// switch emitter on again for two frames
+		epl.update_input(epl.input_slots[0], true)
+
+		graph.update()
+
+		assert.equal(79.21, floatNode.plugin.value)
+
+		graph.update()
+
+		assert.equal(79.21, floatNode.plugin.value)
+	})
 })
 
 	
