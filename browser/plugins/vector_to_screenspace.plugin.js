a16bd5ec126db56169ec5932d837f258a5fafd28
Kaarlo Kananen <kkananen@gmail.com>
2015-11-30 16:28:51 +0000

    fix camera matrix operations so that a valid camera view matrix can be extracted from a three.js camera

--- browser/plugins/vector_to_screenspace.plugin.js
+++ browser/plugins/vector_to_screenspace.plugin.js
@@ -3,42 +3,46 @@ E2.p = E2.plugins["vector_to_screenspace"] = function(core, node)
 	this.desc = 'Transform a vector from world to screenspace given a specified camera.';
 	
 	this.input_slots = [ 
-		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'Input vector (in worldspace) to transform.', def: core.renderer.vector_origin },
-		{ name: 'camera', dt: core.datatypes.CAMERA, desc: 'Camera to use for transformation.', def: core.renderer.camera_screenspace } 
-	];
+		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'Input vector (in worldspace) to transform.' },
+		{ name: 'camera', dt: core.datatypes.CAMERA, desc: 'Camera to use for transformation.' }
+	]
 	
 	this.output_slots = [
-		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'Emits the transformed input vector.', def: core.renderer.vector_origin }
-	];
-};
+		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'Emits the transformed input vector.' }
+	]
+}
 
-E2.p.prototype.update_input = function(slot, data)
-{
-	if(slot.index === 0)
-		this.vector = data;
-	else if(slot.index === 1)
-		this.camera = data;
-};	
+E2.p.prototype.update_input = function(slot, data) {
+	if (slot.index === 0)
+		this.vector = data
+	else if (slot.index === 1)
+		this.camera = data
+}
 
-E2.p.prototype.update_state = function()
-{
-	var m = mat4.create();
-	
-	mat4.multiply(this.camera.projection, this.camera.view, m);
-	mat4.multiplyVec3(m, this.vector, this.transformed);
-};
+E2.p.prototype.update_state = function() {
+	this.camera.updateMatrixWorld()
+	this.camera.updateProjectionMatrix()
+
+	var m = new THREE.Matrix4()
+	this.camera.matrixWorldInverse.getInverse(this.camera.matrixWorld)
+	m.multiplyMatrices(this.camera.projectionMatrix, this.camera.matrixWorldInverse)
+
+	this.transformed.copy(this.vector)
+
+	this.transformed.applyMatrix4(m)
+}
 
 E2.p.prototype.update_output = function(slot)
 {
-	return this.transformed;
-};	
+	return this.transformed
+}
 
 E2.p.prototype.state_changed = function(ui)
 {
 	if(!ui)
 	{
-		this.vector = [0, 0, 0];
-		this.transformed = [0, 0, 0];
-		this.camera = new Camera();
+		this.vector = new THREE.Vector3()
+		this.transformed = new THREE.Vector3()
+		this.camera = new THREE.Camera()
 	}
-};
+}
--- browser/plugins/vector_transform.plugin.js
+++ browser/plugins/vector_transform.plugin.js
@@ -3,12 +3,12 @@ E2.p = E2.plugins["vector_transform"] = function(core, node)
 	this.desc = 'Transform a vector by the supplied matrix.';
 	
 	this.input_slots = [ 
-		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'Input vector to transform.', def: core.renderer.vector_origin },
-		{ name: 'matrix', dt: core.datatypes.MATRIX, desc: 'Transformation matrix to multiply the vector by.', def: core.renderer.matrix_identity } 
+		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'Input vector to transform.' },
+		{ name: 'matrix', dt: core.datatypes.MATRIX, desc: 'Transformation matrix to multiply the vector by.' }
 	];
 	
 	this.output_slots = [
-		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'Emits the transformed input vector.', def: core.renderer.vector_origin }
+		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'Emits the transformed input vector.' }
 	];
 };
 
@@ -22,7 +22,8 @@ E2.p.prototype.update_input = function(slot, data)
 
 E2.p.prototype.update_state = function()
 {
-	this.vector.transformDirection(this.matrix)
+	this.transformed.copy(this.vector)
+	this.transformed.applyMatrix4(this.matrix)
 };
 
 E2.p.prototype.update_output = function(slot)
@@ -34,9 +35,9 @@ E2.p.prototype.state_changed = function(ui)
 {
 	if(!ui)
 	{
-		this.vector = new THREE.Vector3(0, 0, 0)
-		this.transformed = new THREE.Vector3(0, 0, 0)
-		this.matrix = new THREE.Matrix4(0, 0, 0)
+		this.vector = new THREE.Vector3()
+		this.transformed = new THREE.Vector3()
+		this.matrix = new THREE.Matrix4()
 		this.matrix.identity()
 	}
 };
