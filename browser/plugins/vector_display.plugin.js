935a06460311d4f56abc12bc0462b62bb10b2dc5
Jaakko Manninen <kschzt@gmail.com>
2015-12-14 13:17:29 +0100

    fix vector_display plugin

--- browser/plugins/vector_display.plugin.js
+++ browser/plugins/vector_display.plugin.js
@@ -1,61 +1,55 @@
-E2.p = E2.plugins["vector_display"] = function(core, node)
-{
-	this.desc = 'Displays the supplied vector as a three-cell row of values.';
+(function() {
+var VectorDisplay = E2.plugins.vector_display = function(core) {
+	this.desc = 'Displays the supplied vector as a three-cell row of values.'
 	
 	this.input_slots = [ 
 		{ name: 'vector', dt: core.datatypes.VECTOR, desc: 'The vector to be displayed.', def: core.renderer.vector_origin }
-	];
+	]
 	
-	this.output_slots = [];
-};
+	this.output_slots = []
+}
 
-E2.p.prototype.reset = function()
-{
-	this.vec = ['-', '-', '-'];
-	this.update_values();
-};
+VectorDisplay.prototype.reset = function() {
+	this.vec = new THREE.Vector3(0, 0, 0)
+	this.update_values()
+}
 
-E2.p.prototype.create_ui = function()
-{
-	var table = make('table');
-	var row = make('tr');
-	var css = { 'text-align': 'right', 'padding-left': '10px' };
+VectorDisplay.prototype.create_ui = function(){
+	var table = make('table')
+	var row = make('tr')
+	var css = { 'text-align': 'right', 'padding-left': '10px' }
 	
-	this.columns = [make('td'), make('td'), make('td')];
+	this.columns = [make('td'), make('td'), make('td')]
 
-	for(var i = 0; i < 3; i++)
-	{
-		var c = this.columns[i];
+	for(var i = 0; i < 3; i++) {
+		var c = this.columns[i]
 		
-		c.text('-');
-		c.css(css);
-		row.append(c);
+		c.text('-')
+		c.css(css)
+		row.append(c)
 	}
 	
-	table.append(row);
+	table.append(row)
 
-	return table;
-};
+	return table
+}
 
-E2.p.prototype.connection_changed = function(on, conn, slot)
-{
-	if(!on)
-		this.reset(null);
-};
+VectorDisplay.prototype.connection_changed = function(on) {
+	if (!on)
+		this.reset()
+}
 
-E2.p.prototype.update_input = function(slot, data)
-{
-	for(var i = 0; i < 3; i++)
-		this.vec[i] = data[i].toFixed(2);
+VectorDisplay.prototype.update_input = function(slot, data) {
+	this.vec = data
+	this.update_values()
+}
 
-	this.update_values();
-};
-
-E2.p.prototype.update_values = function()
-{
-	if(!this.columns)
-		return;
+VectorDisplay.prototype.update_values = function() {
+	if (!this.columns)
+		return
 	
-	for(var i = 0; i < 3; i++)
-		this.columns[i].text(this.vec[i])
-};
+	this.columns[0].text(this.vec.x)
+	this.columns[1].text(this.vec.y)
+	this.columns[2].text(this.vec.z)
+}
+})()
