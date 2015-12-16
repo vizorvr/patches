4d5acbc93be6454c315202ae9c9918d84d406f9a
Kaarlo Kananen <kkananen@gmail.com>
2015-12-16 12:11:58 +0000

    new inputs for phong material

--- browser/plugins/three_material_phong.plugin.js
+++ browser/plugins/three_material_phong.plugin.js
@@ -21,7 +21,20 @@
 				desc: 'Define shading type. 0 = No Shading, '+
 					'1 = Flat Shading, 2 = Smooth Shading.'
 			},
-		].concat(this.input_slots)
+		].concat(this.input_slots).concat([
+			{	name: 'emissiveMap', dt: core.datatypes.TEXTURE, def: undefined },
+			{	name: 'emissive', dt: core.datatypes.COLOR, def: undefined },
+			{	name: 'specular', dt: core.datatypes.COLOR, def: undefined },
+			{	name: 'shininess', dt: core.datatypes.FLOAT, def: 30 },
+			{	name: 'metal', dt: core.datatypes.BOOL, def: false },
+			{   name: 'lightMapIntensity', dt: core.datatypes.FLOAT, def: 1 },
+			{   name: 'normalScale', dt: core.datatypes.VECTOR, def: new THREE.Vector3(1, 1, 1) },
+			{   name: 'bumpScale', dt: core.datatypes.FLOAT, def: 1 },
+			{   name: 'displacementScale', dt: core.datatypes.FLOAT, def: 1 },
+			{   name: 'displacementBias', dt: core.datatypes.FLOAT, def: 1 },
+			{   name: 'reflectivity', dt: core.datatypes.FLOAT, def: 1 },
+			{   name: 'refractionRatio', dt: core.datatypes.FLOAT, def: 0.98 }
+		])
 	}
 
 	ThreePhongMaterialPlugin.prototype = Object.create(AbstractThreeMaterialPlugin.prototype)
@@ -29,6 +42,17 @@
 	ThreePhongMaterialPlugin.prototype.reset = function() {
 		AbstractThreeMaterialPlugin.prototype.reset.call(this)
 		this.material = new THREE.MeshPhongMaterial()
+	}
+
+	ThreePhongMaterialPlugin.prototype.update_input = function(slot, data) {
+		if (slot.name === 'normalScale' && data !== undefined) {
+			// set Vector2 from Vector3
+			this.material.normalScale.set(data.x, data.y)
+
+			return
+		}
+
+		AbstractThreeMaterialPlugin.prototype.update_input.apply(this, arguments)
 	}
 
 })()
