6e7a19c165b330926e886ae94b6c680f22c1aa40
Kaarlo Kananen <kkananen@gmail.com>
2016-02-18 15:12:17 +0000

    take window.devicePixelRatio into account in cardboard effect

--- browser/scripts/core.js
+++ browser/scripts/core.js
@@ -25,6 +25,12 @@ var URL_GRAPH_FILES = URL_DATA+'graph/'
 
 var E2 = {}
 
+// disable DPDB (device database) fetching for WebVR boilerplate because
+// the list in the device database doesn't have Samsung S6 on the list.
+window.WebVRConfig = window.WebVRConfig || {}
+window.WebVRConfig.NO_DPDB_FETCH = true
+
+
 if (typeof(window) !== 'undefined')
 	window.E2 = E2; // global scope so plugins can access it
 
