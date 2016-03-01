8500093df26dfc103f18f08de23c5bc2a71ecedd
Jaakko Manninen <kschzt@gmail.com>
2016-03-01 22:44:16 +0200

    redispatch queued events after fork snapshot; resolves #1384

--- browser/scripts/editorChannel.js
+++ browser/scripts/editorChannel.js
@@ -215,7 +215,15 @@ EditorChannel.prototype.fork = function(payload) {
 		})
 		.finally(function() {
 			that.forking = false
-			that.processQueue()
+
+			var forkQueue = that.queue.slice()
+			that.queue = []
+
+			while(forkQueue.length) {
+				var dispatch = forkQueue.pop()
+				// re-dispatch queued events after snapshot
+				E2.app.dispatcher.dispatch(dispatch)
+			}
 		})
 }
 
@@ -292,9 +300,18 @@ EditorChannel.prototype.send = function(payload) {
 	if (E2.app.snapshotPending && isEditAction(payload))
 		return this.snapshot()
 
+	if (this.canSend() && !this.queue.length)
+		return this.sendPayload(payload)
+
 	this.queue.push(payload)
 
 	this.processQueue()
+}
+
+EditorChannel.prototype.sendPayload = function(payload) {
+	this.wsChannel.send(
+		payload.channel === 'Global' ? payload.channel : this.channelName,
+		dehydrate(payload))
 }
 
 EditorChannel.prototype.processQueue = function() {
