7aa4391cc4668b417c568654fa7fc7437cfa9ab2
Sakari Lehtonen <sakari@vizor.io>
2016-03-02 16:45:34 +0200

    Get rid of this.params, set defaults in the slots instead and use those to create the material

--- browser/plugins/three_meshline_material.plugin.js
+++ browser/plugins/three_meshline_material.plugin.js
@@ -3,53 +3,42 @@ var ThreeMeshLineMaterialPlugin = E2.plugins.three_meshline_material = function(
 	AbstractThreeMaterialPlugin.apply(this, arguments)
 
 	this.desc = 'THREE.js MeshLine Material'
-	this.material = null
-	this.resolution = new THREE.Vector2(1, 1)
-
-	// MeshLine params, used to create the material
-	this.params = {
-		color: new THREE.Color(0xffffff),
-		opacity: 1.0,
-		transparent: false,
-		resolution: this.resolution,
-		sizeAttenuation: true,
-		lineWidth: 0.01,
-		near: 0.01,
-		far: 1000,
-		depthTest: true,
-		blending: THREE.AdditiveBlending,
-		side: THREE.DoubleSide,
-		wireframe: false
-	}
 
 	// Just override the input slots here
 	// If we want to get the stuff from the material, then would have to concat
 	this.input_slots = [ 
-		{ name: 'color',		dt: core.datatypes.COLOR, def: this.params.color },
-		{ name: 'opacity',		dt: core.datatypes.FLOAT, def: this.params.opacity },
-		{ name: 'transparent',		dt: core.datatypes.BOOL,  def: this.params.transparent },
-		{ name: 'sizeAttenuation',	dt: core.datatypes.BOOL,  def: this.params.sizeAttenuation },
-		{ name: 'lineWidth',		dt: core.datatypes.FLOAT, def: this.params.lineWidth },
-		{ name: 'near',			dt: core.datatypes.FLOAT, def: this.params.near },
-		{ name: 'far',			dt: core.datatypes.FLOAT, def: this.params.far },
-		{ name: 'depthTest',		dt: core.datatypes.BOOL,  def: this.params.depthTest },
-		{ name: 'side',			dt: core.datatypes.FLOAT, def: this.params.side },
-		{ name: 'wireframe',		dt: core.datatypes.BOOL,  def: this.params.wireframe }
+		{ name: 'color',		dt: core.datatypes.COLOR, def: new THREE.Color(0xffffff) },
+		{ name: 'opacity',		dt: core.datatypes.FLOAT, def: 1.0 },
+		{ name: 'transparent',		dt: core.datatypes.BOOL,  def: false },
+		{ name: 'sizeAttenuation',	dt: core.datatypes.BOOL,  def: true },
+		{ name: 'lineWidth',		dt: core.datatypes.FLOAT, def: 0.01 },
+		{ name: 'near',			dt: core.datatypes.FLOAT, def: 0.01 },
+		{ name: 'far',			dt: core.datatypes.FLOAT, def: 1000 },
+		{ name: 'depthTest',		dt: core.datatypes.BOOL,  def: true },
+		{ name: 'side',			dt: core.datatypes.FLOAT, def: THREE.DoubleSide },
+		{ name: 'wireframe',		dt: core.datatypes.BOOL,  def: false }
 	]
-
 	//].concat(this.input_slots)
+	
 	this.output_slots = [{
 		name: 'material',
 		dt: core.datatypes.MATERIAL
 	}]
+	
+	// Needed for the shader
+	this.resolution = new THREE.Vector2(1, 1)
+
+	// Get the parameters from the input_slots
+	var params = {}
+	this.input_slots.map(function(slot) {
+		params[slot.name] = slot.def
+	})
+	params.resolution = this.resolution
+
+	this.material = new THREE.MeshLineMaterial(params)
 }
 
 ThreeMeshLineMaterialPlugin.prototype = Object.create(AbstractThreeMaterialPlugin.prototype)
-
-ThreeMeshLineMaterialPlugin.prototype.reset = function() {
-	AbstractThreeMaterialPlugin.prototype.reset.call(this)
-	this.material = new THREE.MeshLineMaterial(this.params)
-}
 
 ThreeMeshLineMaterialPlugin.prototype.state_changed = function(ui) {
 	// if ui is null, first time called, bind the resize
@@ -59,8 +48,7 @@ ThreeMeshLineMaterialPlugin.prototype.state_changed = function(ui) {
 }
 
 ThreeMeshLineMaterialPlugin.prototype.update_input = function(slot, data) {
-	// Need to update the params also, used to recreate the material
-	this.params[slot.name] = data
+	Plugin.prototype.update_input.apply(this, arguments)
 
 	if ( slot.name === 'transparent' 
 	  || slot.name === 'wireframe' 
@@ -75,6 +63,7 @@ ThreeMeshLineMaterialPlugin.prototype.update_input = function(slot, data) {
 
 ThreeMeshLineMaterialPlugin.prototype.resize = function() {
 	var canvasArea = E2.app.calculateCanvasArea()
+
 	// Need to recalculate the resolution for sizeAttenuation
 	this.resolution.set(canvasArea.width, canvasArea.height)
 	this.material.uniforms.resolution.value = this.resolution
