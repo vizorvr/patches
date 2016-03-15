e5cca0d73a6c6e6168956acebaf2628d2c5db950
Sakari Lehtonen <sakari@psitriangle.net>
2016-03-15 17:25:24 +0200

    Render the outline obj for selected object

--- browser/plugins/three_webgl_renderer.plugin.js
+++ browser/plugins/three_webgl_renderer.plugin.js
@@ -44,6 +44,55 @@
 		}
 	}
 
+	var OUTLINE_PASSES_CREATED = false;
+
+	ThreeWebGLRendererPlugin.prototype.createOutlinePasses = function() {
+		// Outline and mask scenes. For the selected object rendering pass.
+		this.passScenes = {}
+		this.passScenes.mask = new THREE.Scene()
+		this.passScenes.outline = new THREE.Scene()
+
+		// Outline & mask render materials
+		this.passMaterials = {}
+		this.passMaterials.outline = this.createOutlineShaderMaterial(0.03, new THREE.Vector3(0.0, 1.0, 1.0));
+		this.passMaterials.mask = new THREE.MeshBasicMaterial({ color: 0x000000 })
+
+		// Initialize our render passes
+		this.renderPasses = {}
+
+		// This is the normal rendering pass, render the models in the scene
+		this.renderPasses.scene = new THREE.RenderPass(this.scene, this.perspectiveCamera)
+
+		// Render the outline objects. Basically selected object with a bigger radius
+		this.renderPasses.outline = new THREE.RenderPass(this.passScenes.outline, this.perspectiveCamera)
+		this.renderPasses.outline.clear = false
+
+		// Mask phase to mask out the actual object from the highlight object, to create the highlight effect
+		this.renderPasses.mask = new THREE.MaskPass(this.passScenes.mask, this.perspectiveCamera)
+		this.renderPasses.mask.inverse = true
+
+		// Clear the mask ?
+		this.renderPasses.clearMask = new THREE.ClearMaskPass()
+
+		// The copy phase where we copy the resulting renderTarget to the canvas as a fullscreen quad
+		this.renderPasses.copy = new THREE.ShaderPass( THREE.CopyShader )
+		this.renderPasses.copy.renderToScreen = true
+
+		// Create our effect composer
+		this.composer = new THREE.EffectComposer(this.renderer)
+
+		// Need the stencils for the outline
+		this.composer.renderTarget1.stencilBuffer = true
+		this.composer.renderTarget2.stencilBuffer = true
+
+		// Add the passes to our composer
+		this.composer.addPass(this.renderPasses.scene)
+		this.composer.addPass(this.renderPasses.mask)
+		this.composer.addPass(this.renderPasses.outline)
+		this.composer.addPass(this.renderPasses.clearMask)
+		this.composer.addPass(this.renderPasses.copy)
+	}
+
 	ThreeWebGLRendererPlugin.prototype.reset = function() {
 		this.domElement = E2.dom.webgl_canvas[0]
 
@@ -60,22 +109,80 @@
 			0.1,
 			1000)
 
-		// Initialize our render passes
-		this.renderPasses = {}
+		if (OUTLINE_PASSES_CREATED === false) {
+			this.createOutlinePasses()
+			OUTLINE_PASSES_CREATED = true;
+		}
+	}
 
-		// This is the normal rendering pass, render the models in the scene
-		this.renderPasses.scene = new THREE.RenderPass(this.scene, this.perspectiveCamera)
-		this.renderPasses.bloom = new THREE.BloomPass( 1.1 )
-		this.renderPasses.copy = new THREE.ShaderPass( THREE.CopyShader )
-		this.renderPasses.copy.renderToScreen = true
+	// Create our shader material for rendering the simple outline
+	ThreeWebGLRendererPlugin.prototype.createOutlineShaderMaterial = function(offset, color) {
+		var outlineShader = {
+			// TODO: this is too simple
+			// It is just rendering the current material faces with an extended position
+			// resulting in the cube for example missing the corners
+			//
+			// Should just extend the radius of the original object if possible and render with that
+			vertexShader: [
+				"uniform float offset;",
+				"void main() {",
+				"vec4 pos = modelViewMatrix * vec4( position + normal * offset, 1.0 );",
+				"gl_Position = projectionMatrix * pos;",
+				"}"
+			].join("\n"),
 
-		// Create our effect composer
-		this.composer = new THREE.EffectComposer(this.renderer)
-		this.composer.addPass(this.renderPasses.scene)
-		this.composer.addPass(this.renderPasses.bloom)
-		this.composer.addPass(this.renderPasses.copy)
-		//composer.renderTarget1.stencilBuffer = true;
-		//composer.renderTarget2.stencilBuffer = true;
+			fragmentShader: [
+				"uniform vec3 outlineColor;",
+				"void main(){",
+				"gl_FragColor = vec4(outlineColor, 1.0);",
+				"}"
+			].join("\n")
+		};
+
+		var uniforms = { 
+			// The offset amount the outline object is bigger than the selected object
+			offset: { type: "f", value: offset},
+			// Color we are outlining with
+			outlineColor: { type: "v3", value: color },
+		}
+
+		var outlineShaderMaterial = new THREE.ShaderMaterial({
+			uniforms: uniforms, 
+			vertexShader: outlineShader.vertexShader,
+			fragmentShader: outlineShader.fragmentShader
+		})
+
+		return outlineShaderMaterial
+	}
+
+	// Add clones of the object mesh passed in to our mask and outline pass scenes
+	ThreeWebGLRendererPlugin.prototype.addOutlineObj = function(objMesh) {
+		console.log("AddOutlineObj Called");
+
+		var maskMesh = objMesh.clone()
+
+		maskMesh.material = this.passMaterials.mask
+		this.passScenes.mask.add(maskMesh)
+
+		var outlineMesh = objMesh.clone()
+
+		outlineMesh.material = this.passMaterials.outline
+		this.passScenes.outline.add(outlineMesh)
+	}
+
+	ThreeWebGLRendererPlugin.prototype.clearOutlines = function() {
+		var len = this.passScenes.mask.children.length - 1;
+		var obj;
+
+		if (len < 0) return;
+
+		for (var i=len; i>=0; i--) {
+			obj = this.passScenes.mask.children[i];
+			this.passScenes.mask.remove(obj);
+
+			obj = this.passScenes.outline.children[i];
+			this.passScenes.outline.remove(obj);
+		}
 	}
 
 	ThreeWebGLRendererPlugin.prototype.update_input = function(slot, data) {
@@ -118,12 +225,29 @@
 		if (E2.app.worldEditor.isActive()) {
 			E2.app.worldEditor.preRenderUpdate()
 
+			this.clearOutlines();
+
 			// We can get the selected objects from the worldEditor here
 			var selectionTree = E2.app.worldEditor.selectionTree
+
+			if (selectionTree.children.length > 0) {
+				var outlineObj = selectionTree.children[1].children[0]
+				if (outlineObj) {
+					this.addOutlineObj(outlineObj)
+				}
+			}
+
+			// In our selectionTree, we should have the selected object
+			// So .. we should clone this to our maskScene and outlineScene
 			
 			// Render the scene through the world editor camera
-			this.renderPasses.scene.camera = E2.app.worldEditor.getCamera()
+			var camera = E2.app.worldEditor.getCamera()
+
 			this.renderPasses.scene.scene = this.scene
+			this.renderPasses.scene.camera = camera
+			this.renderPasses.mask.camera = camera
+			this.renderPasses.outline.camera = camera
+			this.renderPasses.clearMask.camera = camera
 
 			// Use the composer to render now .. 
 			this.composer.render();
@@ -159,6 +283,10 @@
 
 			if (typeof(E2.app.calculateCanvasArea) !== 'undefined')
 				wh = E2.app.calculateCanvasArea()
+		}
+
+		if (this.composer) {
+			this.composer.reset();
 		}
 
 		this.effect.setSize(wh.width, wh.height)
