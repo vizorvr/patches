fcae5c268ad58d77528142bea7c7680db36ad94a
Jaakko Manninen <kschzt@gmail.com>
2016-07-21 21:03:37 +0300

    wip: fix bootup, skip temp ui creation

--- browser/scripts/application.js
+++ browser/scripts/application.js
@@ -2083,15 +2083,6 @@ Application.prototype.startWithGraph = function(selectedGraphUrl) {
 	function start() {
 		E2.dom.canvas_parent.toggle(that.noodlesVisible)
 
-		// create root graph ui once to calculate its dimensions
-		if (!that.noodlesVisible && E2.core.active_graph !== E2.core.root_graph) {
-			if (E2.core.root_graph.ui)
-				return;
-
-			E2.core.root_graph.create_ui()
-			E2.core.root_graph.destroy_ui()
-		}
-
 		that.startPlaying()
 	}
 
