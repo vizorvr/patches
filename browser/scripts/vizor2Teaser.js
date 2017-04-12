863829d127f126dade2b9ebc3e8830e16ab16a25
Jouko Saastamoinen <joukosaastamoinen@gmail.com>
2017-04-12 15:32:23 +0300

    Code style: Fix indentation

--- browser/scripts/vizor2Teaser.js
+++ browser/scripts/vizor2Teaser.js
@@ -1,6 +1,6 @@
 jQuery('.hero__scroll-down')
-.on('click', function(event) {
-    event.preventDefault();
-    var loc = jQuery('.introduction').offset().top;
-    $("html, body").animate({ scrollTop: loc + 'px' }, 500);
-});
+    .on('click', function(event) {
+        event.preventDefault();
+        var loc = jQuery('.introduction').offset().top;
+        $("html, body").animate({ scrollTop: loc + 'px' }, 500);
+    });
