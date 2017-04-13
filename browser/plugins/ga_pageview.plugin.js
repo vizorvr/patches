eb5e270579e5ea06fa8ea5a8f939dc6de5adf074
Jaakko Manninen <kschzt@gmail.com>
2017-04-13 15:42:17 +0300

    use ES5 in GA plugins

--- browser/plugins/ga_pageview.plugin.js
+++ browser/plugins/ga_pageview.plugin.js
@@ -1,31 +1,30 @@
 (function() {
 
-class GAPageViewPlugin extends Plugin {
-  constructor(core, node) {
-    super(core, node)
-    this.desc = 'Reports a page view (as a hash under the experience URL) to GA any time the input changes. Sets `page` first, then calls `pageview`.'
-    this.input_slots = [{
-      name: 'hash',
-      dt: core.datatypes.TEXT,
-      desc: 'The location hash to report to GA.'
-    }]
-    this.output_slots = []
-    this.currentPage = null
-  }
-  update_input(slot, data) {
-    if (!data || typeof ga === 'undefined')
-      return
+function GAPageViewPlugin() {
+  Plugin.apply(this, arguments)
+  this.desc = 'Reports a page view (as a hash under the experience URL) to GA any time the input changes. Sets `page` first, then calls `pageview`.'
+  this.input_slots = [{
+    name: 'hash',
+    dt: core.datatypes.TEXT,
+    desc: 'The location hash to report to GA.'
+  }]
+  this.output_slots = []
+  this.currentPage = null
+}
+GAPageViewPlugin.prototype = Object.create(Plugin.prototype)
+GAPageViewPlugin.prototype.update_input = function(slot, data) {
+  if (!data || typeof ga === 'undefined')
+    return
 
-    var locationHash = '#' + data
+  var locationHash = '#' + data
 
-    if (this.currentPage === locationHash)
-      return
+  if (this.currentPage === locationHash)
+    return
 
-    this.currentPage = locationHash
+  this.currentPage = locationHash
 
-    ga('clientTracker.set', 'page', window.location.pathname + locationHash)
-    ga('clientTracker.send', 'pageview')
-  }
+  ga('clientTracker.set', 'page', window.location.pathname + locationHash)
+  ga('clientTracker.send', 'pageview')
 }
 
 E2.plugins.ga_pageview = GAPageViewPlugin
--- browser/plugins/google_analytics.plugin.js
+++ browser/plugins/google_analytics.plugin.js
@@ -1,39 +1,38 @@
 (function() {
 
-class GoogleAnalyticsPlugin extends Plugin {
-  constructor(core, node) {
-    super(core, node)
-    this.desc = 'Lets you track views and Vizor events in your own Google Analytics property.'
-    this.input_slots = [{
-      name: 'trackingId',
-      dt: core.datatypes.TEXT,
-      desc: 'Google Analytics property Tracking ID'
-    }]
-    this.output_slots = []
-    this.state = { trackingId: null }
-    this.currentId = null
-  }
-  update_input(slot, data) {
-    this.state.trackingId = data
-  }
-  update_state() {
-    this._createTracker()
-  }
-  _createTracker() {
-    if (!this.state.trackingId
-      || this.currentId === this.state.trackingId
-      || typeof ga === 'undefined')
-      return
+function GoogleAnalyticsPlugin() {
+  Plugin.apply(this, arguments)
+  this.desc = 'Lets you track views and Vizor events in your own Google Analytics property.'
+  this.input_slots = [{
+    name: 'trackingId',
+    dt: core.datatypes.TEXT,
+    desc: 'Google Analytics property Tracking ID'
+  }]
+  this.output_slots = []
+  this.state = { trackingId: null }
+  this.currentId = null
+}
+GoogleAnalyticsPlugin.prototype = Object.create(Plugin.prototype)
+GoogleAnalyticsPlugin.prototype.update_input = function(slot, data) {
+  this.state.trackingId = data
+}
+GoogleAnalyticsPlugin.prototype.update_state = function() {
+  this._createTracker()
+}
+GoogleAnalyticsPlugin.prototype._createTracker = function() {
+  if (!this.state.trackingId
+    || this.currentId === this.state.trackingId
+    || typeof ga === 'undefined')
+    return
 
-    ga('create', this.state.trackingId, 'auto', 'clientTracker')
-    this.currentId = this.state.trackingId
-  }
-  state_changed(ui) {
-    if (!ui)
-      return
+  ga('create', this.state.trackingId, 'auto', 'clientTracker')
+  this.currentId = this.state.trackingId
+}
+GoogleAnalyticsPlugin.prototype.state_changed = function(ui) {
+  if (!ui)
+    return
 
-    this._createTracker()
-  }
+  this._createTracker()
 }
 
 E2.plugins.google_analytics = GoogleAnalyticsPlugin
