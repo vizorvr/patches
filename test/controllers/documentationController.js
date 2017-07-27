1a0ef6060fb8e0104a682b08e55a7256591993e7
Esa Ruoho <esaruoho@EsaVizorMBP.local>
2017-07-27 14:38:01 +0300

    adjusted with and_modulator.md so they both correspond

--- test/controllers/documentationController.js
+++ test/controllers/documentationController.js
@@ -15,9 +15,9 @@ describe('DocumentationController', function() {
 		req.params.pluginName = 'and_modulator'
 
 		res.json = function(data) {
-			assert.equal(data.desc, "<p>Emit true if and only if both inputs are true and false otherwise.</p>\n")
-			assert.deepEqual(data.inputs, [{name: 'a', desc: '<p>The first operand.</p>\n'}, {name: 'b', desc: '<p>The second operand.</p>\n'}])
-			assert.deepEqual(data.outputs, [{name: 'bool', desc: '<p>Emits true if <strong>first</strong> <em>and</em> <strong>second</strong> are true, and false otherwise.</p>\n'}])
+			assert.equal(data.desc, "<p>Emit <strong>True</strong> if <em>and only if</em> both inputs are <strong>True</strong>. <strong>False</strong> otherwise.</p>\n")
+			assert.deepEqual(data.inputs, [{name: 'a', desc: '<p>The first <strong>operand</strong>.</p>\n'}, {name: 'b', desc: '<p>The second <strong>operand</strong>.</p>\n'}])
+			assert.deepEqual(data.outputs, [{name: 'bool', desc: '<p>Emits <strong>True</strong> if <strong>first</strong> <em>and</em> <strong>second</strong> are <strong>True</strong>, and <strong>False</strong> otherwise.</p>\n'}])
 
 			done()
 		}
